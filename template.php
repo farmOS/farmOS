c992b1c9ed9c86bb6b4d36bc88115c11d039e25b
Michael Stenta <mike@mstenta.net>
2015-02-26 14:28:05 -0500

    Wrap the Views exposed form in a Bootstrap collapsible panel.

--- template.php
+++ template.php
@@ -1,5 +1,38 @@
 <?php
 /**
  * @file
- * template.php
+ * Farm theme template.php
  */
+
+/**
+ * Implements hook_form_alter().
+ */
+function farm_theme_form_alter(&$form, &$form_state, $form_id) {
+
+  // Collapse the Views Exposed form
+  if ($form_id == 'views_exposed_form') {
+
+    /**
+     * Wrap the exposed form in a Bootstrap collapsible panel.
+     */
+
+    // Form prefix HTML:
+    $form['#prefix'] = '
+<div class="panel panel-default">
+  <div class="panel-heading" role="tab" id="ViewsFilterSortHeading">
+    <h4 class="panel-title">
+      <a class="collapsed" data-toggle="collapse" href="#ViewsFilterSort" aria-expanded="false" aria-controls="ViewsFilterSort">
+        Filter/Sort
+      </a>
+    </h4>
+  </div>
+  <div id="ViewsFilterSort" class="panel-collapse collapse" role="tabpanel" aria-labelledby="ViewsFilterSortHeading" aria-expanded="false" style="height: 0px;">
+    <div class="panel-body">';
+
+    // Form suffix HTML:
+    $form['#suffix'] = '
+    </div>
+  </div>
+</div>';
+  }
+}
