72a3eef1c4461c9cf2f4917364c24c290c9fd487
Michael Stenta <mike@mstenta.net>
2015-10-03 14:13:30 -0400

    Rename to Farm Access.

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
-FARM MANAGER
-============
+FARM ACCESS
+===========
 
-**This module is DEPRECATED**
+Provides mechanisms for managing farmOS user access permissions.
 
-Use Farm Access instead: http://drupal.org/project/farm_access
+This module is part of the [farmOS](http://drupal.org/project/farm)
+distribution.
+
+DEPENDENCIES
+------------
+
+**Requires Drupal core patch!**
+
+Note that this module depends on a patch to Drupal core that ensures vocabulary
+names are used in the naming of permissions, rather than vocabulary IDs (which
+can vary from site to site).
+
+Here is the patch: http://www.drupal.org/files/issues/995156-5_portable_taxonomy_permissions_0.patch
+
+And the related issue thread: http://drupal.org/node/995156
+
+If you are using the farmOS distribution, this patch is already included.
+
+INSTALLATION
+------------
+
+Install as you would normally install a contributed drupal module. See:
+http://drupal.org/documentation/install/modules-themes/modules-7 for further
+information.
+
+MAINTAINERS
+-----------
+
+Current maintainers:
+ * Michael Stenta (m.stenta) - https://drupal.org/user/581414
+
+This project has been sponsored by:
+ * [Farmier](http://farmier.com)
---
+++ farm_access.api.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by farm_access.
+ *
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * @defgroup farm_access Farm access module integrations.
+ *
+ * Module integrations with the farm_access module.
+ */
+
+/**
+ * @defgroup farm_access_hooks Farm access's hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend farm_access.
+ */
+
+/**
+ * Defines farm access permissions.
+ * Use farm_access_entity_perms() to generate permissions for common entity
+ * types.
+ *
+ * @return array
+ *   Returns an array of farm access permissions.
+ *
+ * @see farm_access_entity_perms()
+ */
+function hook_farm_access_perms() {
+
+  // Build a list of permissions on behalf of the farm_crop module.
+  $types = array(
+    'farm_asset' => array(
+      'planting',
+    ),
+    'log' => array(
+      'farm_harvest',
+      'farm_input',
+      'farm_seeding',
+      'farm_transplanting',
+    ),
+    'taxonomy' => array(
+      'farm_crops',
+      'farm_crop_families',
+    ),
+  );
+  return farm_access_entity_perms($types);
+}
+
+/**
+ * @}
+ */
