1f29d59bd418be0b2f38d7e395387cf5114981d4
Michael Stenta <mike@mstenta.net>
2016-10-17 13:23:37 -0400

    Preserve the sites folder when the codebase is replaced.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,16 +8,32 @@ if $FARMOS_DEV && [ ! -e /var/farmOS/build-farm.make ]; then
   # Clone the farmOS installation profile.
   git clone --branch $FARMOS_DEV_BRANCH https://git.drupal.org/project/farm.git /var/farmOS
 
+  # If the sites folder exists, preserve it by temporarily moving it up one dir.
+  if [ -e /var/www/html/sites ]; then
+    mv /var/www/html/sites /var/www/sites
+  fi
+
   # Build farmOS with Drush.
   rm -rf /var/www/html/* \
   && drush make /var/farmOS/build-farm.make /tmp/farmOS \
   && cp -r /tmp/farmOS/. /var/www/html \
   && rm -r /tmp/farmOS
 
+  # Restore the sites folder.
+  if [ -e /var/www/sites ]; then
+    rm -r /var/www/html/sites \
+    && mv /var/www/sites /var/www/html/sites
+  fi
+
   # Change ownership of the sites folder.
   chown -R www-data:www-data /var/www/html/sites
 
 else
+
+  # If the sites folder exists, preserve it by temporarily moving it up one dir.
+  if [ -e /var/www/html/sites ]; then
+    mv /var/www/html/sites /var/www/sites
+  fi
 
   # Download and unpack farmOS release.
   echo >&2 "Downloading farmOS $FARMOS_VERSION..."
@@ -25,6 +41,12 @@ else
   echo >&2 "Unpacking farmOS $FARMOS_VERSION..."
   rm -rf /var/www/html/*
   tar -xvzf /usr/src/farm-${FARMOS_VERSION}-core.tar.gz -C /var/www/html/ --strip-components=1
+
+  # Restore the sites folder.
+  if [ -e /var/www/sites ]; then
+    rm -r /var/www/html/sites \
+    && mv /var/www/sites /var/www/html/sites
+  fi
 
   # Change ownership of the sites folder.
   chown -R www-data:www-data /var/www/html/sites
