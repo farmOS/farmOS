986e8992732900494369cf807315163b87d188af
Michael Stenta <mike@mstenta.net>
2016-10-21 21:05:00 -0400

    Replace tabs with spaces.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,37 +4,37 @@ set -e
 # Function for archiving the sites folder.
 archive_sites () {
 
-	# If the sites folder exists, preserve it by temporarily archiving it.
-	if [ -e /var/www/html/sites ]; then
-		echo >&2 "Existing sites folder detected. Archiving temporarily..."
-		tar -czf /var/www/html/sites.tar.gz /var/www/html/sites
-	fi
+  # If the sites folder exists, preserve it by temporarily archiving it.
+  if [ -e /var/www/html/sites ]; then
+    echo >&2 "Existing sites folder detected. Archiving temporarily..."
+    tar -czf /var/www/html/sites.tar.gz /var/www/html/sites
+  fi
 }
 
 # Function for restoring the sites folder.
 restore_sites () {
 
-	# Restore the sites folder.
-	if [ -e /var/www/html/sites.tar.gz ]; then
-		echo >&2 "Restoring sites directory..."
-		rm -r /var/www/html/sites \
-		&& tar -xzf /var/www/html/sites.tar.gz -C /var/www/html/ --strip-components=3 \
-		&& rm /var/www/html/sites.tar.gz
-	fi
+  # Restore the sites folder.
+  if [ -e /var/www/html/sites.tar.gz ]; then
+    echo >&2 "Restoring sites directory..."
+    rm -r /var/www/html/sites \
+    && tar -xzf /var/www/html/sites.tar.gz -C /var/www/html/ --strip-components=3 \
+    && rm /var/www/html/sites.tar.gz
+  fi
 
-	# Change ownership of the sites folder.
+  # Change ownership of the sites folder.
   chown -R www-data:www-data /var/www/html/sites
 }
 
 # Function for deleting the farmOS codebase.
 delete_farmos () {
 
-	# Remove the existing farmOS codebase, if it exists.
-	# Exclude sites.tar.gz.
-	if [ -e /var/www/html/index.php ]; then
-		echo >&2 "Removing existing farmOS codebase..."
-		find /var/www/html ! -name 'sites.tar.gz' -mindepth 1 -delete
-	fi
+  # Remove the existing farmOS codebase, if it exists.
+  # Exclude sites.tar.gz.
+  if [ -e /var/www/html/index.php ]; then
+    echo >&2 "Removing existing farmOS codebase..."
+    find /var/www/html ! -name 'sites.tar.gz' -mindepth 1 -delete
+  fi
 }
 
 # Function for downloading and unpacking a farmOS release.
@@ -80,11 +80,11 @@ build_farmos () {
 # Function for determining whether a rebuild is required.
 rebuild_required () {
 
-	# If farm.info doesn't exist, a rebuild is required.
-	if ! [ -e /var/www/html/profiles/farm/farm.info ]; then
-	  echo >&2 "farmOS not detected. Building..."
+  # If farm.info doesn't exist, a rebuild is required.
+  if ! [ -e /var/www/html/profiles/farm/farm.info ]; then
+    echo >&2 "farmOS not detected. Building..."
     return 0
-	fi
+  fi
 
   # Get the current version of farmOS from the installation profile info file.
   CURRENT_VERSION="$(grep 'version' profiles/farm/farm.info | sed 's/version = .\(.*\)./\1/')"
@@ -104,15 +104,15 @@ rebuild_required () {
 # Rebuild farmOS, if necessary.
 if rebuild_required; then
 
-	# Archive the sites folder and delete the farmOS codebase.
-	archive_sites
-	delete_farmos
+  # Archive the sites folder and delete the farmOS codebase.
+  archive_sites
+  delete_farmos
 
-	# Build farmOS.
-	build_farmos
+  # Build farmOS.
+  build_farmos
 
-	# Restore the sites folder.
-	restore_sites
+  # Restore the sites folder.
+  restore_sites
 fi
 
 # Execute the arguments passed into this script.
