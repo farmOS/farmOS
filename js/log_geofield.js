fa2a1ceb1a524bb035fc7e8caa2c8bd092a8ceca
Michael Stenta <mike@mstenta.net>
2017-04-20 16:12:08 -0400

    Update Geofield collapsing JS to work with multiple fields and to only collapse if the field is empty.

--- js/log_geofield.js
+++ js/log_geofield.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.farm_theme_log_geofield = {
     attach: function (context, settings) {
 
-      // Collapse the geofield fieldset by default. We do this in JS
-      // rather than PHP because a PHP collapsed fieldset breaks
-      // the Openlayers Geofield map for some reason. But if we let
+      // Collapse the geofield fieldset by default if it is empty.
+      // We do this in JS rather than PHP because a PHP collapsed fieldset
+      // breaks the Openlayers Geofield map for some reason. But if we let
       // that render first, and then collapse with JS, it works.
       /**
        * @todo
@@ -12,9 +12,13 @@
        * https://www.drupal.org/node/2579009
        */
       setTimeout(function(){
-        var fieldset = $('.field-type-geofield fieldset.collapsible', context);
-        var toggle = $(fieldset.find('[data-toggle=collapse]').data('target'));
-        toggle.collapse('hide');
+        $('.field-type-geofield', context).each(function(index) {
+          if (!$(this).find('.openlayers-geofield-data').val()) {
+            var fieldset = $(this).find('fieldset.collapsible');
+            var toggle = $(fieldset.find('[data-toggle=collapse]').data('target'));
+            toggle.collapse('hide');
+          }
+        });
       }, 1000);
 
     },
