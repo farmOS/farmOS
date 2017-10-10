9cf48f1e40e2e20b2ad5a9f37d3d4c99cf36fa98
Michael Stenta <mike@mstenta.net>
2017-10-10 17:04:04 -0400

    Hide help text and add a help icon to the page title that toggles it on/off.

---
+++ themes/farm_theme/js/help.js
@@ -0,0 +1,14 @@
+(function ($) {
+  Drupal.behaviors.farm_grazing_help = {
+    attach: function(context, settings) {
+      if ($('.region-help', context).length) {
+        var glyphicon = '<span class="glyphicon glyphicon-question-sign" aria-hidden="true" title="Click for more information"></span>';
+        $('.page-header', context).append(' ' + glyphicon);
+        $('.region-help', context).hide();
+        $('.page-header .glyphicon-question-sign', context).click(function() {
+          $('.region-help', context).slideToggle();
+        });
+      }
+    }
+  }
+})(jQuery);
