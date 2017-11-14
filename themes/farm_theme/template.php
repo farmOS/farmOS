bde7bd2cfdf21a4c67dbc61e38b1f19e5803843e
Michael Stenta <mike@mstenta.net>
2017-11-14 15:04:51 -0500

    Style the "Move" VBO action button with a globe icon and green background.

--- themes/farm_theme/template.php
+++ themes/farm_theme/template.php
@@ -184,7 +184,7 @@ function farm_theme_views_bulk_operations_form_alter(&$form, &$form_state, $vbo)
 function farm_theme_bootstrap_colorize_text_alter(&$texts) {
 
   // Colorize VBO action buttons.
-  $texts['matches'][t('Move')] = 'default';
+  $texts['matches'][t('Move')] = 'success';
   $texts['matches'][t('Group')] = 'warning';
   $texts['matches'][t('Done')] = 'success';
   $texts['matches'][t('Not Done')] = 'danger';
@@ -199,7 +199,7 @@ function farm_theme_bootstrap_colorize_text_alter(&$texts) {
 function farm_theme_bootstrap_iconize_text_alter(&$texts) {
 
   // Iconize VBO action buttons.
-  $texts['matches'][t('Move')] = 'move';
+  $texts['matches'][t('Move')] = 'globe';
   $texts['matches'][t('Group')] = 'bookmark';
   $texts['matches'][t('Done')] = 'check';
   $texts['matches'][t('Not Done')] = 'unchecked';
