ba4c29915a0a77cb6d7cfaa5b290a3c927c16205
Michael Stenta <mike@mstenta.net>
2017-11-24 21:58:07 -0500

    Fix menu dividers when farm_plan is not enabled.

--- themes/farm_theme/js/menu.js
+++ themes/farm_theme/js/menu.js
@@ -6,10 +6,12 @@
         var menus = ['assets', 'logs', 'plans'];
         var weights = [0, 100];
         for (var i = 0; i < menus.length; i++) {
-          for (var j = 0; j < weights.length; j++) {
-            if (dividers[menus[i]][weights[j]] !== undefined) {
-              var li = dividers[menus[i]][weights[j]];
-              $('.nav .dropdown .' + menus[i], context).siblings('.dropdown-menu').children('li:eq(' + li + ')').after($('<li class="divider"></li>'));
+          if (dividers[menus[i]] !== undefined) {
+            for (var j = 0; j < weights.length; j++) {
+              if (dividers[menus[i]][weights[j]] !== undefined) {
+                var li = dividers[menus[i]][weights[j]];
+                $('.nav .dropdown .' + menus[i], context).siblings('.dropdown-menu').children('li:eq(' + li + ')').after($('<li class="divider"></li>'));
+              }
             }
           }
         }
