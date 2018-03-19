0eb2d5fd611faac2d786ef95ec09b48744427b67
Michael Stenta <mike@mstenta.net>
2018-03-19 13:08:51 -0400

    Allow modules to provide flags.

---
+++ modules/farm/farm_flags/farm_flags.api.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by farm_flags.
+ *
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * @defgroup farm_flags Farm flag module integrations.
+ *
+ * Module integrations with the farm_flags module.
+ */
+
+/**
+ * @defgroup farm_flags_hooks Farm flag's hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend farm_flags.
+ */
+
+/**
+ * Provide a list of flags that can be applied to records for filtering.
+ *
+ * @return array
+ *   Returns an associative array of flags, with machine name and translatable.
+ */
+function hook_farm_flags() {
+  return array(
+    'priority' => t('Priority'),
+    'review' => t('Needs Review'),
+    'organic' => t('Organic'),
+    'notorganic' => t('Not Organic'),
+  );
+}
+
+/**
+ * @}
+ */
