91cb17bb7f62acbe360b8c21b87b0b3e5eefe4c8
Michael Stenta <mike@mstenta.net>
2018-05-30 15:30:19 -0400

    Allow other modules to help extract geometries from entities via hook_farm_map_entity_geometries().

---
+++ modules/farm/farm_map/farm_map.api.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by farm_map.
+ *
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * @defgroup farm_map Farm map module integrations.
+ *
+ * Module integrations with the farm_map module.
+ */
+
+/**
+ * @defgroup farm_map_hooks Farm map's hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend farm_map.
+ */
+
+/**
+ * Extract geometries from an entity.
+ *
+ * @param $entity_type
+ *   The entity type machine name.
+ * @param $entity
+ *   The entity object.
+ *
+ * @return array
+ *   Return an array of geometry strings in WKT format. An associative array
+ *   is allowed, and the keys can be used to differentiate multiple geometries
+ *   from the same entity.
+ */
+function hook_farm_map_entity_geometries($entity_type, $entity) {
+  $geometries = array();
+
+  // Find geometry in the standard geofield.
+  if (empty($entity->field_farm_geofield[LANGUAGE_NONE][0]['geom'])) {
+    $geometries[] = $entity->field_farm_geofield[LANGUAGE_NONE][0]['geom'];
+  }
+
+  return $geometries;
+}
+
+/**
+ * @}
+ */
