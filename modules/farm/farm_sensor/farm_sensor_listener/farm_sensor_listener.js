c522dd64853cb9b074aca3bc81ae65dae3b0b8c5
Michael Stenta <mike@mstenta.net>
2018-11-06 11:01:00 -0500

    Issue #2493511: Graph sensor data

---
+++ modules/farm/farm_sensor/farm_sensor_listener/farm_sensor_listener.js
@@ -0,0 +1,44 @@
+(function ($) {
+  Drupal.behaviors.farm_area_generate = {
+    attach: function (context, settings) {
+
+      // Iterate through the graphs.
+      for (var i = 0; i < settings.farm_sensor_listener.graphs.length; i++) {
+
+        // Get the graph name, id, and data.
+        var name = settings.farm_sensor_listener.graphs[i]['name'];
+        var id = settings.farm_sensor_listener.graphs[i]['id'];
+        var data = settings.farm_sensor_listener.graphs[i]['data'];
+
+        // Initialize variables.
+        var dates=[];
+        var values=[];
+
+        // Iterate through the data and put it into the arrays.
+        for (var j = 0; j < data.length; j++) {
+          var date = new Date(data[j].timestamp * 1000).toISOString();
+          dates.push(date);
+          values.push(data[j][name]);
+        }
+
+        // Assemble variables for plotly.
+        var graph_data=[{
+          x: dates,
+          y: values,
+          name: name,
+          type: 'scatter'
+        }];
+        var layout = {
+          title: name,
+          height: 400,
+          xaxis: { title: 'date' },
+          yaxis: { title: name }
+        };
+
+        // Draw the graph to the element.
+        element = document.getElementById(id);
+        Plotly.newPlot(element, graph_data, layout);
+      }
+    }
+  };
+}(jQuery));
