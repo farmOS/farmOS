0eea90dec071312044ae04d785bb3a2f1ca7698d
Michael Stenta <mike@mstenta.net>
2019-07-24 11:05:32 -0400

    Refactor NRCS Soil Survey to use a farmOS-map behavior.

---
+++ modules/farm/farm_soil/farm_soil_nrcs/farmOS.map.behaviors.soil_survey.js
@@ -0,0 +1,16 @@
+(function () {
+  farmOS.map.behaviors.soil_survey = {
+    attach: function (instance) {
+      var options = {
+        title: 'NRCS Soil Survey',
+        url: 'https://sdmdataaccess.nrcs.usda.gov/Spatial/SDM.wms',
+        params: {
+          LAYERS: 'MapunitPoly',
+          VERSION: '1.1.1',
+        },
+        visible: false,
+      };
+      instance.addLayer('wms', options);
+    }
+  };
+}());
