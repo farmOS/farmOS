61df12ed895dcf22818bd79547df779b38c164ed
Paul Weidner <paul.weidner@gmail.com>
2019-08-26 20:58:45 -0700

    Add the average animal gain per day to group report

--- modules/farm/farm_livestock/farm_livestock_weight/farm_livestock_weight_group_graph.js
+++ modules/farm/farm_livestock/farm_livestock_weight/farm_livestock_weight_group_graph.js
@@ -14,8 +14,12 @@
         var data = settings.farm_livestock_report.graphs[i]['data'];
 
         // Initialize variables.
-        var dates=[];
-        var values=[];
+        var average_dates=[];
+        var average_values=[];
+        var average_value_text=[];
+
+        var gain_dates=[];
+        var gain_values=[];
 
         // Initialize the default_units
         var default_units = '';
@@ -26,27 +30,50 @@
             default_units = data[d].units;
           }
           var date = new Date(d).toISOString();
-          dates.push(date);
-          values.push(data[d].total_weight/data[d].animal_count);
+          average_dates.push(date);
+          average_values.push(data[d].total_weight/data[d].animal_count);
+          average_value_text.push(data[d].animal_count + " animals weighed");
+
+          if (data[d].hasOwnProperty('gain')) {
+            gain_dates.push(date);
+            gain_values.push(data[d].gain);
+          }
         }
 
         // Assemble variables for plotly.
-        var graph_data=[{
-          x: dates,
-          y: values,
-          name: name,
+        var average_data={
+          x: average_dates,
+          y: average_values,
+          text: average_value_text,
+          name: 'Average Weight',
           type: 'scatter'
-        }];
+        };
+
+        var gain_data={
+          x: gain_dates,
+          y: gain_values,
+          yaxis: 'y2',
+          name: 'Average Daily Gain',
+          type: 'scatter'
+        };
+
+        var all_data = [average_data, gain_data];
+
         var layout = {
           title: name,
           height: 400,
           xaxis: { title: 'Date' },
-          yaxis: { title: 'Weight (' + default_units + ')' }
+          yaxis: { title: 'Average Weight (' + default_units + ')' },
+          yaxis2: {
+            title: 'Average Daily Gain (' + default_units + ')',
+            overlaying: 'y',
+            side: 'right'
+          }
         };
 
         // Draw the graph to the element.
         element = document.getElementById(id);
-        Plotly.newPlot(element, graph_data, layout);
+        Plotly.newPlot(element, all_data, layout);
       }
     }
   };
