d7c67a130c3162cb649ee353a16fc30e6c1b389c
Michael Stenta <mike@mstenta.net>
2019-09-11 15:21:03 -0400

    Add all area type layers to farmOS-map.

---
+++ modules/farm/farm_area/js/farmOS.map.behaviors.areas.js
@@ -0,0 +1,27 @@
+(function () {
+  farmOS.map.behaviors.areas = {
+    attach: function (instance) {
+
+      // Add layers for each area type.
+      if (Drupal.settings.farm_map.behaviors.areas.layers !== undefined) {
+        var layers = Drupal.settings.farm_map.behaviors.areas.layers;
+        for (var i = 0; i < layers.length; i++) {
+          var opts = {
+            title: layers[i].label,
+            url: layers[i].url,
+            color: layers[i].style,
+          };
+          var layer = instance.addLayer('geojson', opts);
+
+          // If zoom is true, zoom to all vector layers when they load.
+          if (Drupal.settings.farm_map.behaviors.areas.zoom) {
+            var source = layer.getSource();
+            source.on('change', function () {
+              instance.zoomToVectors();
+            });
+          }
+        }
+      }
+    }
+  };
+}());
