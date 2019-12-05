c8c89d195e47e08460e230290daba65fb6edd1d6
Michael Stenta <mike@mstenta.net>
2019-12-05 16:43:15 -0500

    Set onFocusOnly: true when initializing maps, so that the user must click on the map before scroll zoom works.

--- modules/farm/farm_map/js/farm_map.js
+++ modules/farm/farm_map/js/farm_map.js
@@ -2,10 +2,12 @@
   Drupal.behaviors.farm_map = {
     attach: function (context, settings) {
       var units = Drupal.settings.farm_map.units;
+      var interactions = { onFocusOnly: true };
       if (settings.farm_map.maps) {
         settings.farm_map.maps.forEach(function (target) {
           $('#' + target, context).once('farm-map', function () {
-            farmOS.map.create(target, { units: units });
+            $('#' + target, context).attr('tabIndex', 0);
+            farmOS.map.create(target, { units: units, interactions: interactions });
           });
         });
       }
