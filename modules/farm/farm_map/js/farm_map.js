cc5664473cbb6f47ea7c6296b0fe324df97384b4
Michael Stenta <mike@mstenta.net>
2019-12-05 16:43:03 -0500

    Pass system of measurement into farmOS-map.

--- modules/farm/farm_map/js/farm_map.js
+++ modules/farm/farm_map/js/farm_map.js
@@ -1,10 +1,11 @@
 (function ($) {
   Drupal.behaviors.farm_map = {
     attach: function (context, settings) {
+      var units = Drupal.settings.farm_map.units;
       if (settings.farm_map.maps) {
         settings.farm_map.maps.forEach(function (target) {
           $('#' + target, context).once('farm-map', function () {
-            farmOS.map.create(target, {});
+            farmOS.map.create(target, { units: units });
           });
         });
       }
