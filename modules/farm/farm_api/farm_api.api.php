16d498c27b6b1deb11a8b3147f82ad9b40c37231
Paul Weidner <paul.weidner@gmail.com>
2020-01-16 14:21:12 -0800

    Update hook_farm_info api example.

--- modules/farm/farm_api/farm_api.api.php
+++ modules/farm/farm_api/farm_api.api.php
@@ -28,11 +28,33 @@
  *   Returns an array of farm information.
  */
 function hook_farm_info() {
-  global $base_url, $conf;
+  global $base_url, $conf, $user;
+
+  // Info items can be added simply:
   $info = array(
     'name' => $conf['site_name'],
     'url' => $base_url,
   );
+
+  //  Or an entire array of info can be added simply:
+  if (!empty($user->uid)) {
+    $info['user'] = array(
+      'uid' => $user->uid,
+      'name' => $user->name,
+      'mail' => $user->mail,
+    );
+  }
+
+  // Or, they can be arrays with 'info' and 'scope' keys. The 'info' is what
+  // will be included in the farmOS API info array. The 'scope' is an OAuth2
+  // scope that will be checked for access. This scope must be defined in
+  // the farmos_api_client OAuth2 Server. This allows some information to be
+  // available to OAuth2-authenticated services without a full user log in.
+  $info['foo'] = array(
+    'info' => 'bar',
+    'scope' => 'access_foo',
+  );
+
   return $info;
 }
 
