18e86f955021a727932559f447b0b4361c6581ce
Michael Stenta <mike@mstenta.net>
2020-02-02 14:43:41 -0500

    Refactor Drupal.behaviors.farm_map so that Drupal.settings.farm_map.maps is not needed.

--- modules/farm/farm_map/js/farm_map.js
+++ modules/farm/farm_map/js/farm_map.js
@@ -1,16 +1,18 @@
 (function ($) {
   Drupal.behaviors.farm_map = {
     attach: function (context, settings) {
-      var units = Drupal.settings.farm_map.units;
-      var interactions = { onFocusOnly: true };
-      if (settings.farm_map.maps) {
-        settings.farm_map.maps.forEach(function (target) {
-          $('#' + target, context).once('farm-map', function () {
-            $('#' + target, context).attr('tabIndex', 0);
-            farmOS.map.create(target, { units: units, interactions: interactions });
-          });
+      var options = {
+        units: Drupal.settings.farm_map.units,
+        interactions: {
+          onFocusOnly: true
+        },
+      };
+      $('.farm-map', context).each(function (index, element) {
+        $(element).once('farm-map', function () {
+          $(element).attr('tabIndex', 0);
+          farmOS.map.create($(element).attr('id'), options);
         });
-      }
+      });
     }
   };
 }(jQuery));
