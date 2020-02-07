164709c1698134b4dc5b8fbec3fb9b70ec692765
Michael Stenta <mike@mstenta.net>
2020-02-07 11:50:16 -0500

    Add cluster support to the popup behavior.

--- modules/farm/farm_map/js/farmOS.map.behaviors.popup.js
+++ modules/farm/farm_map/js/farmOS.map.behaviors.popup.js
@@ -7,6 +7,23 @@
         var content = '';
         var feature = instance.map.forEachFeatureAtPixel(event.pixel, function(feature, layer) { return feature; });
         if (feature) {
+
+          // If the feature is a cluster, then create a list of names and add it
+          // to the overall feature's description.
+          var features = feature.get('features');
+          if (features !== undefined) {
+            var names = [];
+            features.forEach(function (item) {
+              if (item.get('name') !== undefined) {
+                names.push(item.get('name'));
+              }
+            });
+            if (names.length !== 0) {
+              feature.set('description', '<ul><li>' + names.join('</li><li>') + '</li></ul>');
+            }
+            feature.set('name', names.length + ' item(s):');
+          }
+
           var name = feature.get('name') || '';
           var description = feature.get('description') || '';
           if (name !== '' || description !== '') {
