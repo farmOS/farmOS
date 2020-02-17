76bedcb7799378b43af97fe15f3caf44924dc646
Michael Stenta <mike@mstenta.net>
2020-02-17 13:40:50 -0500

    Make the Mapbox Satellite layer visible by default.

--- modules/farm/farm_map/farm_map_mapbox/js/farmOS.map.behaviors.mapbox.js
+++ modules/farm/farm_map/farm_map_mapbox/js/farmOS.map.behaviors.mapbox.js
@@ -3,15 +3,15 @@
     attach: function (instance) {
       var key = Drupal.settings.farm_map.behaviors.mapbox.api_key;
       this.addMapboxLayer(instance, 'Mapbox Outdoors', 'mapbox.outdoors', key);
-      this.addMapboxLayer(instance, 'Mapbox Satellite', 'mapbox.satellite', key);
+      this.addMapboxLayer(instance, 'Mapbox Satellite', 'mapbox.satellite', key, true);
     },
-    addMapboxLayer: function (instance, title, tileset, key) {
+    addMapboxLayer: function (instance, title, tileset, key, visible = false) {
       var opts = {
         title: title,
         url: 'https://api.mapbox.com/v4/' + tileset + '/{z}/{x}/{y}.png?access_token=' + key,
         group: 'Base layers',
         base: true,
-        visible: false,
+        visible: visible,
       };
       instance.addLayer('xyz', opts);
     }
