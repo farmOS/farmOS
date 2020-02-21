27c83e92d5d2a836bcf32ce7ba7dbd01f9e1c89e
Michael Stenta <mike@mstenta.net>
2020-02-20 23:30:17 -0500

    Make sure farmOS.map.behaviors.geofield runs after farmOS.map.behaviors.wkt.

--- modules/farm/farm_map/farm_map_geofield/js/farmOS.map.behaviors.geofield.js
+++ modules/farm/farm_map/farm_map_geofield/js/farmOS.map.behaviors.geofield.js
@@ -5,6 +5,8 @@
         $('#' + instance.target).parents('.field-widget-farm-map-geofield').find('textarea').val(wkt);
       });
     },
-    weight: 100,
+
+    // Make sure this runs after farmOS.map.behaviors.wkt.
+    weight: 101,
   };
 }(jQuery));
