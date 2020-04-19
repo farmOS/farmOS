d1a26fc7e8fd7b8bffc52202466ee64bec0c0906
Michael Stenta <mike@mstenta.net>
2020-04-19 11:03:46 -0400

    Initial Dockerfile. Copied from farmOS 7.x-1.x. Modified to build codebase with Composer create-project farmos/project.

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# If the sites directory is empty, copy from /tmp/sites.
+if ! [ "$(ls -A /var/www/html/web/sites/)" ]; then
+  echo "farmOS sites directory not detected. Copying from pre-built codebase in the Docker image."
+  cp -rp /tmp/sites/. /var/www/html/web/sites
+fi
+
+# Execute the arguments passed into this script.
+echo "Attempting: $@"
+exec "$@"
