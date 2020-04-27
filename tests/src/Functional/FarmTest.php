b3370dd4d8c16d26a04be15bd59b18a2d30f9d4f
Michael Stenta <mike@mstenta.net>
2020-04-27 16:07:41 -0400

    Add FarmTest class: Test integrated functionality of the farmOS profile.

---
+++ tests/src/Functional/FarmTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\Tests\farm\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests integrated functionality of the farmOS profile.
+ *
+ * @group farm
+ */
+class FarmTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'farm';
+
+  /**
+   * Tests integrated functionality of the farmOS profile.
+   */
+  public function testFarm() {
+
+    // Test that the profile was installed.
+    $this->assertSame($this->profile, $this->container->getParameter('install_profile'));
+  }
+
+}
