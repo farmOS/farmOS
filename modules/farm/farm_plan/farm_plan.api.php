ce25c3c73f94b02873d1f2dacd901a6b50efd17e
Michael Stenta <mike@mstenta.net>
2020-05-06 11:46:42 -0400

    Allow record relationships to define a human-readable label.

--- modules/farm/farm_plan/farm_plan.api.php
+++ modules/farm/farm_plan/farm_plan.api.php
@@ -30,6 +30,7 @@
  * @return array
  *   Returns an array of record types with the database table and field names
  *   used to store relationships, using the following keys:
+ *     - label: The human-readable label for this record type.
  *     - entity_type: The Drupal entity type.
  *     - entity_pk: The column name of the entity type's primary key.
  *     - table: The table that links the record to the plan.
@@ -38,6 +39,7 @@
 function hook_farm_plan_record_relationships() {
   return array(
     'my_plan_log' => array(
+      'label' => t('Log'),
       'entity_type' => 'log',
       'entity_pk' => 'id',
       'table' => 'my_plan_log',
