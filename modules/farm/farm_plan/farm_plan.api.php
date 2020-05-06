0ff7aca7aacb3861891ebcbd605ddf7ec85be142
Michael Stenta <mike@mstenta.net>
2020-05-06 09:44:37 -0400

    Allow modules to define their own plan record relationship tables with hook_farm_plan_record_relationships().

---
+++ modules/farm/farm_plan/farm_plan.api.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by farm_plan.
+ *
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * @defgroup farm_plan Farm plan module integrations.
+ *
+ * Module integrations with the farm_plan module.
+ */
+
+/**
+ * @defgroup farm_plan_hooks Farm plan's hooks
+ * @{
+ * Hooks that can be implemented by other modules in order to extend farm_plan.
+ */
+
+/**
+ * Define farm plan record relationships.
+ *
+ * This will be used to automatically integrate with Views, and create
+ * constraints on linked records.
+ *
+ * @return array
+ *   Returns an array of record types with the database table and field names
+ *   used to store relationships, using the following keys:
+ *     - entity_type: The Drupal entity type.
+ *     - entity_pk: The column name of the entity type's primary key.
+ *     - table: The table that links the record to the plan.
+ *     - field: The column name in the table that links to the record's PK.
+ */
+function hook_farm_plan_record_relationships() {
+  return array(
+    'my_plan_log' => array(
+      'entity_type' => 'log',
+      'entity_pk' => 'id',
+      'table' => 'my_plan_log',
+      'field' => 'log_id',
+    ),
+  );
+}
+
+/**
+ * @}
+ */
