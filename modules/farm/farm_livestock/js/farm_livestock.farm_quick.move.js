6f231744f18bf7dba8a4012667c66f5cffbc3b79
paul121 <paul.weidner@gmail.com>
2020-06-26 11:23:41 -0700

    Create an editable Movement layer that updates with area geometry.

--- modules/farm/farm_livestock/js/farm_livestock.farm_quick.move.js
+++ modules/farm/farm_livestock/js/farm_livestock.farm_quick.move.js
@@ -7,4 +7,13 @@
       farmOS.map.behaviors.move.previewCurrentLocation(wkt);
     }
   }
+
+  // Define a Drupal ajax command for loading the Movement To area wkt
+  // from a hidden input field and preview it as an editable layer in the map.
+  Drupal.ajax.prototype.commands.updateMovementLayer = function() {
+    var wkt = $('#next-location input[name="move[area][next_location]"]').val();
+    if (wkt) {
+      farmOS.map.behaviors.move.updateMovementLayer(wkt);
+    }
+  }
 }(jQuery));
--- modules/farm/farm_movement/js/farmOS.map.behaviors.move.js
+++ modules/farm/farm_movement/js/farmOS.map.behaviors.move.js
@@ -10,10 +10,17 @@
        */
       this.instance = instance;
 
-      // When features are changed in the map, drop the WKT into the data field.
-      instance.edit.wktOn('featurechange', function(wkt) {
-        $('#' + instance.target).parent().find('textarea').val(wkt);
-      });
+      // Create an editable movement layer.
+      // Init as an empty vector layer, the layer can be recreated
+      // as an editable WKT layer later.
+      var opts = {
+        title: 'Movement',
+        color: 'blue',
+      };
+      this.movementLayer = this.instance.addLayer('vector', opts);
+
+      // Make the layer editable.
+      this.instance.addBehavior('edit', { layer: this.movementLayer });
     },
 
     // Update the assets current location map layer.
@@ -35,6 +42,43 @@
       };
       this.currentLocationLayer = this.instance.addLayer('wkt', opts);
       this.instance.zoomToLayer(this.currentLocationLayer);
+    },
+
+    // Recreate the Movement map layer.
+    updateMovementLayer: function (wkt) {
+
+      // Remove current location layer.
+      if (this.movementLayer) {
+        this.instance.map.removeLayer(this.movementLayer);
+        this.movementLayer = null;
+      }
+
+      // Create current location layer with the WKT.
+      // Do not put the layer inside a group, because map.removeLayer() (used
+      // above) does not recurse into layer groups.
+      var opts = {
+        title: 'Movement',
+        color: 'blue',
+        wkt: wkt,
+      };
+      this.movementLayer = this.instance.addLayer('wkt', opts);
+
+      // Make the layer editable.
+      this.instance.addBehavior('edit', { layer: this.movementLayer });
+
+      // Zoom to the new layer.
+      this.instance.zoomToLayer(this.movementLayer);
+
+      // Save the map instance ID.
+      const target = this.instance.target;
+
+      // Update the data field with the selected areas WKT.
+      $('#' + target).parent().find('textarea').val(wkt);
+
+      // When features are changed in the map, drop the WKT into the data field.
+      this.instance.edit.wktOn('featurechange', function(wkt) {
+        $('#' + target).parent().find('textarea').val(wkt);
+      });
     },
 
     // Make sure this runs after farmOS.map.behaviors.wkt.
