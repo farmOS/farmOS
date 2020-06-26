b77da615a402c2d6d0c6edd00094d0bfed09ffac
Michael Stenta <mike@mstenta.net>
2020-06-26 10:07:02 -0400

    Only apply farm_theme_map_collapse JS behavior to fieldsets that contain a map.

--- themes/farm_theme/js/map_panel.js
+++ themes/farm_theme/js/map_panel.js
@@ -5,13 +5,15 @@
       // If a map is inside a collapsed fieldset, update the map size when the
       // fieldset is expanded.
       $('fieldset.collapsible', context).once('map-collapse', function (index, element) {
-        $(element).on('shown.bs.collapse', function () {
-          var mapId = $('.farm-map', element).attr('id');
-          var index = farmOS.map.targetIndex(mapId);
-          if (index !== -1) {
-            farmOS.map.instances[index].map.updateSize();
-          }
-        });
+        if ($('.farm-map', element).length) {
+          $(element).on('shown.bs.collapse', function () {
+            var mapId = $('.farm-map', element).attr('id');
+            var index = farmOS.map.targetIndex(mapId);
+            if (index !== -1) {
+              farmOS.map.instances[index].map.updateSize();
+            }
+          });
+        }
       });
     },
   };
