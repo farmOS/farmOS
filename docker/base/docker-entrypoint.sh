26c004d6a12635f1d65da4582a0037aa0d034af5
Michael Stenta <mike@mstenta.net>
2020-08-08 18:14:13 -0400

    Build farmOS in /var/farmOS instead of /var/www/farmOS.

--- docker/base/docker-entrypoint.sh
+++ docker/base/docker-entrypoint.sh
@@ -4,19 +4,19 @@ set -e
 ###
 # This entrypoint script will check to see if certain directories were mounted
 # as volumes (and are therefore empty), and will copy files from the pre-built
-# farmOS codebase in /var/www/farmOS to populate them.
+# farmOS codebase in /var/farmOS to populate them.
 ###
 
-# If the /var/www/html directory is empty, copy from /var/www/farmOS.
+# If the /var/www/html directory is empty, copy from /var/farmOS.
 if ! [ "$(ls -A /var/www/html/)" ]; then
   echo "farmOS codebase not detected. Copying from pre-built files in the Docker image."
-  cp -rp /var/www/farmOS/. /var/www/html
+  cp -rp /var/farmOS/. /var/www/html
 fi
 
-# If the sites directory is empty, copy from /var/www/farmOS/web/sites.
+# If the sites directory is empty, copy from /var/farmOS/web/sites.
 if ! [ "$(ls -A /var/www/html/web/sites/)" ]; then
   echo "farmOS sites directory not detected. Copying from pre-built files in the Docker image."
-  cp -rp /var/www/farmOS/web/sites/. /var/www/html/web/sites
+  cp -rp /var/farmOS/web/sites/. /var/www/html/web/sites
 fi
 
 # Execute the arguments passed into this script.
