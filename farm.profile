e1dfe45cdbead34b33f4e6156f3ec7d149310b08
Michael Stenta <mike@mstenta.net>
2020-08-15 11:39:00 -0400

    Add an install task for selecting+installing farmOS modules.

---
+++ farm.profile
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * farmOS installation profile.
+ */
+
+/**
+ * Define farmOS modules that can be installed.
+ *
+ * @return array
+ *   Returns an array with two sub-arrays: 'default' and 'optional'. Default
+ *   modules will be selected for installation by default, and optional modules
+ *   will require the user to select them for installation.
+ */
+function farm_modules() {
+  return [
+    'default' => [
+      'farm_activity' => t('Activity logs'),
+      'farm_observation' => t('Observation logs'),
+      'farm_input' => t('Input logs'),
+      'farm_harvest' => t('Harvest logs'),
+    ],
+    'optional' => [
+      'farm_seeding' => t('Seeding logs'),
+      'farm_transplanting' => t('Transplanting logs'),
+      'farm_lab_test' => t('Lab test logs'),
+      'farm_maintenance' => t('Maintenance logs'),
+      'farm_medical' => t('Medical logs'),
+      'farm_purchase' => t('Purchase logs'),
+      'farm_sale' => t('Sale logs'),
+    ],
+  ];
+}
---
+++ src/Form/FarmModulesForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\farm\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * farmOS modules form.
+ *
+ * @ingroup farm
+ */
+class FarmModulesForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'farm_modules_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Set the form title.
+    $form['#title'] = $this->t('Enable modules');
+
+    // Load the list of available modules.
+    $modules = farm_modules();
+
+    // Allow user to choose which high-level farm modules to install.
+    $module_options = array_merge($modules['default'], $modules['optional']);
+
+    // Default modules will be selected by default.
+    $module_defaults = array_keys($modules['default']);
+
+    // Module checkboxes.
+    $form['modules'] = [
+      '#title' => t('farmOS Modules'),
+      '#title_display' => 'invisible',
+      '#type' => 'checkboxes',
+      '#description' => t('Select the farmOS modules that you would like installed by default.'),
+      '#options' => $module_options,
+      '#default_value' => $module_defaults,
+    ];
+
+    // Submit button.
+    $form['actions'] = ['#type' => 'actions'];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save and continue'),
+      '#button_type' => 'primary',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $modules = array_filter($form_state->getValue('modules'));
+    \Drupal::state()->set('farm.install_modules', $modules);
+  }
+
+}
