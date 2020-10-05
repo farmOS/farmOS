4702995a960e1636faa97a8a9a71e7b03b3e8efb
paul121 <paul.weidner@gmail.com>
2020-10-05 11:34:28 -0700

    Include the farm profile version in meta.farm.version.

--- modules/core/api/src/Controller/FarmEntryPoint.php
+++ modules/core/api/src/Controller/FarmEntryPoint.php
@@ -2,11 +2,15 @@
 
 namespace Drupal\farm_api\Controller;
 
+use Drupal\Core\Extension\ProfileExtensionList;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\jsonapi\Controller\EntryPoint;
 use Drupal\jsonapi\JsonApiResource\JsonApiDocumentTopLevel;
 use Drupal\jsonapi\JsonApiResource\NullIncludedData;
 use Drupal\jsonapi\JsonApiResource\ResourceObjectData;
 use Drupal\jsonapi\ResourceResponse;
+use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Extend the core jsonapi EntryPoint controller.
@@ -16,6 +20,39 @@ use Drupal\jsonapi\ResourceResponse;
  * @ingroup farm
  */
 class FarmEntryPoint extends EntryPoint {
+
+  /**
+   * Farm profile info.
+   *
+   * @var mixed[]
+   */
+  protected $farmProfileInfo;
+
+  /**
+   * EntryPoint constructor.
+   *
+   * @param \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface $resource_type_repository
+   *   The resource type repository.
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   The current user.
+   * @param \Drupal\Core\Extension\ProfileExtensionList $profile_extension_list
+   *   The profile extension list service.
+   */
+  public function __construct(ResourceTypeRepositoryInterface $resource_type_repository, AccountInterface $user, ProfileExtensionList $profile_extension_list) {
+    parent::__construct($resource_type_repository, $user);
+    $this->farmProfileInfo = $profile_extension_list->getExtensionInfo('farm');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('jsonapi.resource_type.repository'),
+      $container->get('current_user'),
+      $container->get('extension.list.profile')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -34,6 +71,7 @@ class FarmEntryPoint extends EntryPoint {
     // Add a "farm" object to meta.
     $meta['farm'] = [
       'name' => $this->config('system.site')->get('name'),
+      'version' => $this->farmProfileInfo['version'],
     ];
 
     // Build a new response.
