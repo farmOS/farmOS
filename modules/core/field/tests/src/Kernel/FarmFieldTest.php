926fbb900eef936202eb1d150ca1a9df2b997b12
Michael Stenta <mike@mstenta.net>
2020-11-17 07:13:29 -0500

    Test that base fields are added to asset and log types.

---
+++ modules/core/field/tests/src/Kernel/FarmFieldTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\entity\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the bundle plugin API.
+ *
+ * @group farm_field
+ */
+class FarmFieldTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'farm';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'asset',
+    'log',
+    'farm_field',
+    'farm_field_test',
+  ];
+
+  /**
+   * Tests the farmOS base fields are added to entities.
+   */
+  public function testPluginBundles() {
+
+    // Load the entity field manager.
+    /** @var \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager */
+    $entity_field_manager = $this->container->get('entity_field.manager');
+
+    // Load asset field storage definitions.
+    $fields = $entity_field_manager->getFieldStorageDefinitions('asset');
+
+    // Confirm that all fields defined in farm_field_asset_base_fields() exist.
+    $this->assertArrayHasKey('data', $fields);
+    $this->assertArrayHasKey('file', $fields);
+    $this->assertArrayHasKey('image', $fields);
+    $this->assertArrayHasKey('notes', $fields);
+    $this->assertArrayHasKey('parent', $fields);
+
+    // Load log field storage definitions.
+    $fields = $entity_field_manager->getFieldStorageDefinitions('log');
+
+    // Confirm that all fields defined in farm_field_log_base_fields() exist.
+    $this->assertArrayHasKey('asset', $fields);
+    $this->assertArrayHasKey('category', $fields);
+    $this->assertArrayHasKey('data', $fields);
+    $this->assertArrayHasKey('file', $fields);
+    $this->assertArrayHasKey('geometry', $fields);
+    $this->assertArrayHasKey('image', $fields);
+    $this->assertArrayHasKey('notes', $fields);
+    $this->assertArrayHasKey('owner', $fields);
+  }
+
+}
