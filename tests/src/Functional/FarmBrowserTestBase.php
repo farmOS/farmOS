11c2f324e194082d51d2ba0df7e76fd42d79fcdd
Michael Stenta <mike@mstenta.net>
2020-11-20 11:32:44 -0500

    Issue #3183739: Functional tests are installing all default farmOS modules
    This is a temporary first step, but not a proper solution to the issue.

--- tests/src/Functional/FarmBrowserTestBase.php
+++ tests/src/Functional/FarmBrowserTestBase.php
@@ -13,4 +13,19 @@ class FarmBrowserTestBase extends BrowserTestBase {
    * {@inheritdoc}
    */
   protected $profile = 'farm';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+
+    // Set a global farm_test variable and then delegate to the parent setUp().
+    // This is a temporary hack to prevent optional default farmOS modules from
+    // being installed via the profile's hook_install_tasks().
+    // @see farm_install_modules()
+    // @todo https://www.drupal.org/project/farm/issues/3183739
+    $GLOBALS['farm_test'] = TRUE;
+    parent::setUp();
+  }
+
 }
