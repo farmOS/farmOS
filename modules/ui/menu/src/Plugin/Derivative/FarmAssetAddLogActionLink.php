b06248623b704cbd361fe1bd0b9dec2a81871f98
paul121 <paul.weidner@gmail.com>
2020-12-09 10:43:52 -0800

    Create an action link to add each log type from the asset view.

---
+++ modules/ui/menu/src/Plugin/Derivative/FarmAssetAddLogActionLink.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\farm_ui_menu\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+
+/**
+ * Provides action links to create logs from the asset view.
+ */
+class FarmAssetAddLogActionLink extends DeriverBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $links = [];
+
+    // Add links for each log type.
+    $bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo('log');
+    foreach ($bundles as $type => $info) {
+      $links['farm.asset.add_log.' . $type] = [
+        'title' => 'Add ' . $info['label'],
+        'route_name' => 'entity.log.add_form',
+        'appears_on' => [
+          'entity.asset.canonical',
+        ],
+        'route_parameters' => ['log_type' => $type],
+      ] + $base_plugin_definition;
+    }
+
+    return $links;
+  }
+
+}
