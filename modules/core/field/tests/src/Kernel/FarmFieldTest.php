a797c5134376de92e405a5ed69dc962513496a58
Michael Stenta <mike@mstenta.net>
2020-12-10 05:52:08 -0500

    Revert "Remove geometry from farm_field test."
    This reverts commit 3b3549bda9a6f4cce9b28d887d6dba6e00ccda0e.

--- modules/core/field/tests/src/Kernel/FarmFieldTest.php
+++ modules/core/field/tests/src/Kernel/FarmFieldTest.php
@@ -54,6 +54,7 @@ class FarmFieldTest extends KernelTestBase {
     $this->assertArrayHasKey('category', $fields);
     $this->assertArrayHasKey('data', $fields);
     $this->assertArrayHasKey('file', $fields);
+    $this->assertArrayHasKey('geometry', $fields);
     $this->assertArrayHasKey('image', $fields);
     $this->assertArrayHasKey('notes', $fields);
     $this->assertArrayHasKey('owner', $fields);
