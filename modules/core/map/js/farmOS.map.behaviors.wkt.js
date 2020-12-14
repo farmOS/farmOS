c7a16d0f5dfc16e0a84285a3be9080a08ddbbf39
paul121 <paul.weidner@gmail.com>
2020-12-14 09:17:26 -0800

    Create the map WKT behavior.

---
+++ modules/core/map/js/farmOS.map.behaviors.wkt.js
@@ -0,0 +1,44 @@
+(function () {
+  farmOS.map.behaviors.wkt = {
+    attach: function (instance) {
+
+      // If WKT was set, create a layer.
+      if (drupalSettings.farm_map[instance.target].wkt) {
+        var wkt = drupalSettings.farm_map[instance.target].wkt;
+        var type = 'vector';
+        var opts = {
+          title: 'Geometry',
+          color: 'orange',
+        };
+        if (wkt !== '' && wkt !== 'GEOMETRYCOLLECTION EMPTY') {
+          type = 'wkt';
+          opts.wkt = wkt;
+        }
+        var layer = instance.addLayer(type, opts);
+      }
+
+      // If edit is true, enable drawing controls.
+      if (drupalSettings.farm_map[instance.target].behaviors.wkt.edit) {
+        if (layer !== undefined) {
+          instance.addBehavior('edit', { layer: layer });
+        } else {
+          instance.addBehavior('edit');
+          var layer = instance.edit.layer;
+        }
+
+        // Add the snappingGrid behavior.
+        instance.addBehavior('snappingGrid');
+      }
+
+      // Enable the line/polygon measure behavior.
+      instance.addBehavior('measure', { layer: layer });
+
+      // If zoom is true and the layer has features, zoom to them.
+      // Otherwise, zoom to all vectors.
+      if (drupalSettings.farm_map[instance.target].behaviors.wkt.zoom && layer !== undefined) {
+        instance.zoomToLayer(layer);
+      }
+    },
+    weight: 100,
+  };
+}());
