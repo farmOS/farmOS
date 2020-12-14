1c840507c1b648d371daf837481ed1a43671a201
Michael Stenta <mike@mstenta.net>
2020-12-14 11:48:55 -0500

    Define AssetLocationInterface.

---
+++ modules/core/location/src/AssetLocationInterface.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\farm_location;
+
+use Drupal\asset\Entity\AssetInterface;
+use Drupal\log\Entity\LogInterface;
+
+/**
+ * Asset location logic.
+ */
+interface AssetLocationInterface {
+
+  /**
+   * Check if an asset is located within other location assets.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The Asset entity.
+   *
+   * @return bool
+   *   Returns TRUE if the asset has a location, FALSE otherwise.
+   */
+  public function hasLocation(AssetInterface $asset): bool;
+
+  /**
+   * Check if an asset has geometry.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The Asset entity.
+   *
+   * @return bool
+   *   Returns TRUE if the asset has geometry, FALSE otherwise.
+   */
+  public function hasGeometry(AssetInterface $asset): bool;
+
+  /**
+   * Get location assets that an asset is located within.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The Asset entity.
+   *
+   * @return array
+   *   Returns an array of assets.
+   */
+  public function getLocation(AssetInterface $asset): array;
+
+  /**
+   * Get an asset's geometry.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The Asset entity.
+   *
+   * @return string
+   *   Returns a WKT string.
+   */
+  public function getGeometry(AssetInterface $asset): string;
+
+  /**
+   * Find the latest movement log that references an asset.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The asset entity.
+   *
+   * @return \Drupal\log\Entity\LogInterface|null
+   *   A log entity, or NULL if no logs were found.
+   */
+  public function getMovementLog(AssetInterface $asset): ?LogInterface;
+
+}
