4cc9c6c2a384f14647b2f05f883e58129c1baa96
Michael Stenta <mike@mstenta.net>
2020-12-19 05:33:08 -0500

    Add a current location field to assets that is computed based on a movements.

---
+++ modules/core/location/src/Field/AssetLocationItemList.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\farm_location\Field;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+
+/**
+ * Computes the current location value for assets.
+ */
+class AssetLocationItemList extends EntityReferenceFieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   * Computes the current location value for the asset.
+   */
+  protected function computeValue() {
+
+    // Get the asset entity.
+    $entity = $this->getEntity();
+
+    // Get the asset's current locations.
+    $locations = \Drupal::service('asset.location')->getLocation($entity);
+
+    // Update the assets current location values to match.
+    // @todo Invalidate/disable the entity cache for JSONAPI.
+    // @todo Cache this field computation.
+    foreach ($locations as $delta => $location) {
+      if (!empty($location->id())) {
+        $value = ['target_id' => $location->id()];
+        $this->list[$delta] = $this->createItem($delta, $value);
+      }
+    }
+  }
+
+}
