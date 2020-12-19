eecedd97a4e98958dc5be6ee111b536971ba6c02
Michael Stenta <mike@mstenta.net>
2020-12-19 05:23:20 -0500

    Move current_geometry to a base field.

--- modules/core/entity/src/Plugin/Asset/AssetType/FarmAssetType.php
+++ modules/core/entity/src/Plugin/Asset/AssetType/FarmAssetType.php
@@ -3,7 +3,6 @@
 namespace Drupal\farm_entity\Plugin\Asset\AssetType;
 
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\farm_location\AssetGeometryItemList;
 
 /**
  * Provides a farmOS asset type base class.
@@ -11,26 +10,5 @@ use Drupal\farm_location\AssetGeometryItemList;
 class FarmAssetType extends AssetTypeBase {
 
   use StringTranslationTrait;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildFieldDefinitions() {
-    $fields = [];
-
-    // Current geometry computed field.
-    $options = [
-      'type' => 'geofield',
-      'label' => $this->t('Current geometry'),
-      'computed' => AssetGeometryItemList::class,
-      'hidden' => 'form',
-      'weight' => [
-        'view' => 95,
-      ],
-    ];
-    $fields['current_geometry'] = $this->farmFieldFactory->bundleFieldDefinition($options);
-
-    return $fields;
-  }
 
 }
