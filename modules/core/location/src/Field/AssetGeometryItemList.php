17f8aa22907e6745a2f6766b4406e7508f0fea01
Michael Stenta <mike@mstenta.net>
2020-12-23 07:54:25 -0500

    Refactor AssetGeometryItemList to use asset.location service.

--- modules/core/location/src/Field/AssetGeometryItemList.php
+++ modules/core/location/src/Field/AssetGeometryItemList.php
@@ -20,65 +20,13 @@ class AssetGeometryItemList extends FieldItemList {
     // Get the asset entity.
     $entity = $this->getEntity();
 
-    // If its a location, copy its intrinsic geometry.
-    // @todo Should this be checking 'fixed' instead?
-    if ($entity->get('location')->value) {
-      $geom = $entity->get('geometry')->value ?? '';
-      $this->list[0] = $this->createItem(0, $geom);
-      return;
-    }
-
-    // Query logs.
-    // @todo use the farm.log_query service.
-    $query = \Drupal::entityTypeManager()->getStorage('log')->getQuery();
-
-    // Add a tag.
-    $query->addTag('farm.location.asset_geofield');
-
-    // Sort by timestamp and then log ID, descending.
-    $query->sort('timestamp', 'DESC');
-    $query->sort('id', 'DESC');
-
-    // Limit to logs that aren't in the future.
-    $query->condition('timestamp', \Drupal::time()->getCurrentTime(), '<=');
-
-    // Limit to logs that are done.
-    $query->condition('status', 'done');
-
-    // Limit to logs that reference the asset.
-    $query->condition('asset.entity.id', $entity->id());
-
-    // Limit to logs that are a movement.
-    $query->condition('movement', TRUE);
-
-    // Limit to the single latest log.
-    $query->range(0, 1);
-
-    $result = $query->execute();
-
-    // Bail if no logs are found.
-    if (empty($result)) {
-      return;
-    }
-
-    // Get the first log.
-    /** @var \Drupal\log\Entity\LogInterface $log */
-    $log = \Drupal::entityTypeManager()->getStorage('log')->load(reset($result));
-
-    // Bail if the log was not loaded.
-    if (empty($log)) {
-      return;
-    }
-
-    // Load the log.location service.
-    /** @var \Drupal\farm_location\LogLocationInterface $log_location */
-    $log_location = \Drupal::service('log.location');
+    // Get the asset geometry.
+    $geometry = \Drupal::service('asset.location')->getGeometry($entity);
 
     // Update the assets current geometry value to match.
     // @todo Invalidate/disable the entity cache for JSONAPI.
     // @todo Cache this field computation.
-    $geom = $log_location->getGeometry($log);
-    $this->list[0] = $this->createItem(0, $geom);
+    $this->list[0] = $this->createItem(0, $geometry);
   }
 
 }
