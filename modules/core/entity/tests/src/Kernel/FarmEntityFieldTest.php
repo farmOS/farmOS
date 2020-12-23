c4a54eaa43576b213d36fa6f79ce69e9fb09e533
Michael Stenta <mike@mstenta.net>
2020-12-23 14:18:22 -0500

    Rename asset location field to is_location.

--- modules/core/entity/tests/src/Kernel/FarmEntityFieldTest.php
+++ modules/core/entity/tests/src/Kernel/FarmEntityFieldTest.php
@@ -60,7 +60,7 @@ class FarmEntityFieldTest extends KernelTestBase {
     $this->assertArrayHasKey('image', $fields);
     $this->assertArrayHasKey('intrinsic_geometry', $fields);
     $this->assertArrayHasKey('is_fixed', $fields);
-    $this->assertArrayHasKey('location', $fields);
+    $this->assertArrayHasKey('is_location', $fields);
     $this->assertArrayHasKey('notes', $fields);
     $this->assertArrayHasKey('parent', $fields);
 
