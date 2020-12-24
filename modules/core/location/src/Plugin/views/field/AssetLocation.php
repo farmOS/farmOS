2bb5e442a7092dd596cc76d08668d72141fc26a8
Michael Stenta <mike@mstenta.net>
2020-12-24 13:58:07 -0500

    Add current location field to Asset View.

---
+++ modules/core/location/src/Plugin/views/field/AssetLocation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\farm_location\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\EntityField;
+
+/**
+ * A field that displays asset location.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("asset_location")
+ */
+class AssetLocation extends EntityField {
+
+}
