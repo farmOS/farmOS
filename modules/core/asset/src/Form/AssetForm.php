e5360a80115576c04e2b757551a5ef7fb948b092
Michael Stenta <mike@mstenta.net>
2020-12-31 09:20:12 -0500

    Always redirect to the Asset entity in AssetForm::save().

--- modules/core/asset/src/Form/AssetForm.php
+++ modules/core/asset/src/Form/AssetForm.php
@@ -18,13 +18,7 @@ class AssetForm extends ContentEntityForm {
   public function save(array $form, FormStateInterface $form_state) {
     parent::save($form, $form_state);
     $this->messenger()->addMessage($this->t('Saved the %label asset.', ['%label' => $this->entity->label()]));
-    $account = $this->currentUser();
-    if ($account->hasPermission('administer assets')) {
-      $form_state->setRedirectUrl($this->entity->toUrl('collection'));
-    }
-    else {
-      $form_state->setRedirectUrl($this->entity->toUrl());
-    }
+    $form_state->setRedirectUrl($this->entity->toUrl());
   }
 
 }
