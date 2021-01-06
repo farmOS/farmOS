ea235f2207a7470259d15a1c0bb00ce1af804e47
paul121 <paul.weidner@gmail.com>
2021-01-06 15:20:51 -0800

    Render a map with asset geometries as an attachment before asset views.

---
+++ modules/ui/views/src/EventSubscriber/MapRenderEventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\farm_ui_views\EventSubscriber;
+
+use Drupal\farm_map\Event\MapRenderEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * An event subscriber for the MapRenderEvent.
+ */
+class MapRenderEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      MapRenderEvent::EVENT_NAME => 'onMapRender',
+    ];
+  }
+
+  /**
+   * React to the MapRenderEvent.
+   *
+   * @param \Drupal\farm_map\Event\MapRenderEvent $event
+   *   The MapRenderEvent.
+   */
+  public function onMapRender(MapRenderEvent $event) {
+
+    // Add the asset type layers behavior to the asset_list map.
+    if ($event->getMapType()->id() == 'asset_list') {
+      $event->addBehavior('asset_type_layers');
+    }
+  }
+
+}
