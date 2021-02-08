bff5c84fe34b87563ed26179ca8d5c9ecef5cc35
paul121 <paul.weidner@gmail.com>
2021-02-08 13:38:57 -0800

    Update the map size when the toolbar is toggled.

--- modules/core/map/js/farm_map.js
+++ modules/core/map/js/farm_map.js
@@ -19,6 +19,26 @@
           };
         });
       });
+
+      // Add an event listener to update the map size when the Gin toolbar is toggled.
+      if (context === document) {
+        document.addEventListener('toolbar-toggle', function(e) {
+
+          // Only continue if map instances are provided.
+          if (typeof farmOS !== 'undefined' && farmOS.map.instances !== 'undefined') {
+
+            // Set a timeout so the computed CSS properties are applied
+            // before updating the map size.
+            setTimeout(function () {
+              // Update the map size of all map instances.
+              farmOS.map.instances.forEach(function (instance) {
+                instance.map.updateSize();
+              });
+
+            }, 200);
+          }
+        });
+      }
     }
   };
 }(Drupal));
