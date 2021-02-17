02de4ba117be05da849f512f1742be17d88eec86
paul121 <paul.weidner@gmail.com>
2021-02-16 17:12:38 -0800

    Deny access to the entity type add form.

---
+++ modules/core/entity/src/Routing/DefaultHtmlRouteProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\farm_entity\Routing;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\entity\Routing\DefaultHtmlRouteProvider as EntityDefaultHtmlRouteProvider;
+
+/**
+ * Deny access to the entity type add form.
+ *
+ * New entity types of entities with bundle plugins cannot be created in the UI.
+ *
+ * @See https://www.drupal.org/project/farm/issues/3196423
+ */
+class DefaultHtmlRouteProvider extends EntityDefaultHtmlRouteProvider {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getAddFormRoute(EntityTypeInterface $entity_type) {
+    $route = parent::getAddFormRoute($entity_type);
+    $route->setRequirement('_access', 'FALSE');
+    return $route;
+  }
+
+}
