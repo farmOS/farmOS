bbf8866a2717fe7d52217dd2d878c14bff27478e
Michael Stenta <mike@mstenta.net>
2021-03-01 11:15:28 -0500

    Override the getMovementLog() method of the asset.location service to take group membership into account.

--- modules/asset/group/src/GroupAssetLocation.php
+++ modules/asset/group/src/GroupAssetLocation.php
@@ -2,12 +2,14 @@
 
 namespace Drupal\farm_group;
 
+use Drupal\asset\Entity\AssetInterface;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\farm_location\AssetLocation;
 use Drupal\farm_location\AssetLocationInterface;
 use Drupal\farm_location\LogLocationInterface;
 use Drupal\farm_log\LogQueryFactoryInterface;
+use Drupal\log\Entity\LogInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -52,6 +54,53 @@ class GroupAssetLocation extends AssetLocation implements AssetLocationInterface
       $container->get('datetime.time'),
       $container->get('group.membership')
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMovementLog(AssetInterface $asset): ?LogInterface {
+
+    // Delegate to the parent service to get the latest movement log that
+    // references this asset.
+    $latest_movement = parent::getMovementLog($asset);
+
+    // Remember the latest movement id and timestamp.
+    $latest_id = 0;
+    $latest_timestamp = 0;
+    if (!empty($latest_movement)) {
+      $latest_id = $latest_movement->id();
+      $latest_timestamp = $latest_movement->get('timestamp')->value;
+    }
+
+    // Get the groups that this asset is assigned to.
+    $groups = $this->groupMembership->getGroup($asset);
+
+    // If there are groups, iterate through them.
+    if (!empty($groups)) {
+      foreach ($groups as $group) {
+
+        // Get the latest movement log that references the group.
+        $group_movement = parent::getMovementLog($group);
+
+        // If the group doesn't have a movement, skip it.
+        if (empty($group_movement)) {
+          continue;
+        }
+
+        // If the group's movement is the latest, replace the latest movement.
+        $group_movement_id = $group_movement->id();
+        $group_movement_timestamp = $group_movement->get('timestamp')->value;
+        if (($group_movement_id > $latest_id) && ($group_movement_timestamp >= $latest_timestamp)) {
+          $latest_movement = $group_movement;
+          $latest_id = $group_movement_id;
+          $latest_timestamp = $group_movement_timestamp;
+        }
+      }
+    }
+
+    // Return the latest movement log.
+    return $latest_movement;
   }
 
 }
