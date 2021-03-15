6642f2bc08c87e61f6b60bce5fe567651a0f7a96
paul121 <paul.weidner@gmail.com>
2021-03-15 09:13:39 -0700

    Add bundle plugins for asset types used in tests.

---
+++ modules/core/location/tests/modules/farm_location_test/src/Plugin/Asset/AssetType/Location.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_location_test\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the test location asset type.
+ *
+ * @AssetType(
+ *   id = "location",
+ *   label = @Translation("Location"),
+ * )
+ */
+class Location extends FarmAssetType {
+
+}
