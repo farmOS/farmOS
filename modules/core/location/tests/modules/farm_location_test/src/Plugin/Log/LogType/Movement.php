eed6503455cdf2bec45a6f26ce9cd9e3fd9eff68
paul121 <paul.weidner@gmail.com>
2021-03-15 13:00:53 -0700

    Add bundle plugin for movement log type used in tests.

---
+++ modules/core/location/tests/modules/farm_location_test/src/Plugin/Log/LogType/Movement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_location_test\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+
+/**
+ * Provides the test movement log type.
+ *
+ * @LogType(
+ *   id = "movement",
+ *   label = @Translation("Movement"),
+ * )
+ */
+class Movement extends FarmLogType {
+
+}
