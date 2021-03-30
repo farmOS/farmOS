c14d5053f09d7ae4a83e8e61592743862f1b3d9e
paul121 <paul.weidner@gmail.com>
2021-03-30 11:17:40 -0700

    LegacyListener extends the Basic data stream type.

--- modules/core/data_stream/legacy/src/Plugin/DataStream/DataStreamType/LegacyListener.php
+++ modules/core/data_stream/legacy/src/Plugin/DataStream/DataStreamType/LegacyListener.php
@@ -2,25 +2,16 @@
 
 namespace Drupal\farm_sensor_listener\Plugin\DataStream\DataStreamType;
 
-use Drupal\Component\Serialization\Json;
-use Drupal\Core\Database\Connection;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\data_stream\DataStreamStorageInterface;
 use Drupal\data_stream\Entity\DataStream;
 use Drupal\data_stream\Entity\DataStreamInterface;
-use Drupal\data_stream\Plugin\DataStream\DataStreamType\DataStreamTypeBase;
+use Drupal\data_stream\Plugin\DataStream\DataStreamType\Basic;
 use Drupal\data_stream\Traits\DataStreamPrivateKeyAccess;
 use Drupal\entity\BundleFieldDefinition;
 use Drupal\farm_sensor_listener\LegacySensorApiInterface;
 use Drupal\fraction\Fraction;
-use Drupal\jsonapi\Exception\UnprocessableHttpEntityException;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 /**
  * Provides the legacy listener data stream type.
@@ -30,7 +21,7 @@ use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
  *   label = @Translation("Legacy listener"),
  * )
  */
-class LegacyListener extends DataStreamTypeBase implements DataStreamStorageInterface, LegacySensorApiInterface {
+class LegacyListener extends Basic implements DataStreamStorageInterface, LegacySensorApiInterface {
 
   use DataStreamPrivateKeyAccess;
 
@@ -40,42 +31,6 @@ class LegacyListener extends DataStreamTypeBase implements DataStreamStorageInte
    * @var string
    */
   protected $tableName = 'data_stream_legacy';
-
-  /**
-   * A database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $connection;
-
-  /**
-   * Constructs a \Drupal\Component\Plugin\PluginBase object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Database\Connection $connection
-   *   The database connection.
-   */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, Connection $connection) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->connection = $connection;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('database'),
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -98,6 +53,50 @@ class LegacyListener extends DataStreamTypeBase implements DataStreamStorageInte
     $fields['public_key'] = $field;
 
     return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+
+    // Inherit the data_stream_basic views data config.
+    $data[$this->tableName] = $data['data_stream_basic'];
+    unset($data['data_stream_basic']);
+
+    // Add the additional name field.
+    $data[$this->tableName]['name'] = [
+      'title' => $this->t('Name'),
+      'help' => $this->t('Name of the value reading.'),
+      'field' => [
+        'id' => 'standard',
+        'click sortable' => TRUE,
+      ],
+      'sort' => [
+        'id' => 'standard',
+      ],
+      'filter' => [
+        // @todo Use/create a filter that displays available "names".
+        'id' => 'standard',
+      ],
+    ];
+
+    // Add a legacy_listener_data relationship to the data_stream_data table
+    // that references the data_stream_legacy table.
+    $data['data_stream_data']['legacy_listener_data'] = [
+      'title' => $this->t('Legacy listener data'),
+      'help' => $this->t('Legacy listener data stream data.'),
+      'relationship' => [
+        'base' => 'data_stream_legacy',
+        'base field' => 'id',
+        'field' => 'id',
+        'id' => 'standard',
+        'label' => $this->t('Legacy listener data'),
+      ],
+    ];
+
+    return $data;
   }
 
   /**
@@ -144,87 +143,7 @@ class LegacyListener extends DataStreamTypeBase implements DataStreamStorageInte
    * {@inheritdoc}
    */
   public function apiHandleLegacyRequest(DataStreamInterface $stream, Request $request) {
-
-    // Get request method.
-    $method = $request->getMethod();
-
-    // Handle GET request.
-    if ($method == Request::METHOD_GET) {
-
-      // Bail if the sensor is not public and no private_key is provided.
-      if (!$stream->isPublic() && !$this->requestHasValidPrivateKey($stream, $request)) {
-        throw new AccessDeniedHttpException();
-      }
-
-      return $this->apiGet($stream, $request);
-    }
-
-    // Handle POST request.
-    if ($method == Request::METHOD_POST) {
-      if (!$this->requestHasValidPrivateKey($stream, $request)) {
-        throw new AccessDeniedHttpException();
-      }
-      return $this->apiPost($stream, $request);
-    }
-
-    // Else bail.
-    throw new MethodNotAllowedHttpException([]);
-  }
-
-  /**
-   * Handle API GET requests.
-   *
-   * @param \Drupal\data_stream\Entity\DataStreamInterface $stream
-   *   The data stream.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The request.
-   *
-   * @return \Symfony\Component\HttpFoundation\JsonResponse
-   *   The response.
-   */
-  protected function apiGet(DataStreamInterface $stream, Request $request) {
-
-    $params = $request->query->all();
-
-    $max_limit = 100000;
-
-    $limit = $max_limit;
-    if (isset($params['limit'])) {
-      $limit = $params['limit'];
-
-      // Bail if more than the max is requested.
-      // Only allow 100k max data points to prevent exhausting PHP's memory,
-      // which is a potential DDoS vector.
-      if ($limit > $max_limit) {
-        throw new UnprocessableHttpEntityException();
-      }
-    }
-    $params['limit'] = $limit;
-
-    $data = $this->storageGet($stream, $params);
-    return JsonResponse::create($data);
-  }
-
-  /**
-   * Handle API POST requests.
-   *
-   * @param \Drupal\data_stream\Entity\DataStreamInterface $stream
-   *   The data stream.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The request.
-   *
-   * @return \Symfony\Component\HttpFoundation\Response
-   *   The response.
-   */
-  protected function apiPost(DataStreamInterface $stream, Request $request) {
-    $data = Json::decode($request->getContent());
-    $success = $this->storageSave($stream, $data);
-
-    if (!$success) {
-      throw new BadRequestHttpException();
-    }
-
-    return Response::create('', Response::HTTP_CREATED);
+    return $this->apiHandleRequest($stream, $request);
   }
 
   /**
@@ -354,7 +273,7 @@ class LegacyListener extends DataStreamTypeBase implements DataStreamStorageInte
       $row['value_numerator'] = $fraction->getNumerator();
       $row['value_denominator'] = $fraction->getDenominator();
 
-      // Enter the reading into the {data_stream_data_storage} table.
+      // Enter the reading into the database.
       $this->connection->insert($this->tableName)
         ->fields($row)
         ->execute();
