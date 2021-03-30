58d2631eadf0580282f6ff070d5a6ae621ab6953
paul121 <paul.weidner@gmail.com>
2021-03-30 10:10:18 -0700

    Move data_stream field to sensor bundle plugin.

--- modules/asset/sensor/src/Plugin/Asset/AssetType/Sensor.php
+++ modules/asset/sensor/src/Plugin/Asset/AssetType/Sensor.php
@@ -14,4 +14,23 @@ use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
  */
 class Sensor extends FarmAssetType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+
+    // Data stream field.
+    $options = [
+      'type' => 'entity_reference',
+      'label' => $this->t('Data stream'),
+      'description' => $this->t('Data streams provided by this sensor.'),
+      'target_type' => 'data_stream',
+      'multiple' => TRUE,
+    ];
+    $fields['data_stream'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    return $fields;
+  }
+
 }
