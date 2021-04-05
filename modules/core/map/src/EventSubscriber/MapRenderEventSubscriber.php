07b06b3a5ff0ee928d8807c09fa935c3247031a3
Michael Stenta <mike@mstenta.net>
2021-04-05 10:20:30 -0400

    Move subscriber logic for adding dashboard map asset layers to farm_ui_map.

--- modules/core/map/src/EventSubscriber/MapRenderEventSubscriber.php
+++ modules/core/map/src/EventSubscriber/MapRenderEventSubscriber.php
@@ -2,36 +2,15 @@
 
 namespace Drupal\farm_map\EventSubscriber;
 
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\farm_map\Event\MapRenderEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * An event subscriber for the MapRenderEvent.
  *
- * Adds the wkt and geofield behaviors to necessary maps.
+ * Adds default behaviors to maps.
  */
 class MapRenderEventSubscriber implements EventSubscriberInterface {
-
-  use StringTranslationTrait;
-
-  /**
-   * Asset types.
-   *
-   * @var \Drupal\asset\Entity\AssetTypeInterface[]
-   */
-  protected $assetTypes;
-
-  /**
-   * MapRenderEventSubscriber Constructor.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
-    $this->assetTypes = $entity_type_manager->getStorage('asset_type')->loadMultiple();
-  }
 
   /**
    * {@inheritdoc}
@@ -81,59 +60,6 @@ class MapRenderEventSubscriber implements EventSubscriberInterface {
       // behavior IDs). Each behavior could provide schema for its valid
       // setting options.
       $settings[$event->getMapTargetId()]['behaviors']['popup']['enabled'] = FALSE;
-      $event->addSettings($settings);
-    }
-
-    // Add asset layers to dashbaord map.
-    if ($event->getmapType()->id() == 'dashboard') {
-
-      $layers = [];
-
-      // Define common layer properties.
-      $group = $this->t('Location assets');
-      $filters = [
-        'is_location' => 1,
-      ];
-
-      // Add layer for all asset types are locations by default.
-      foreach ($this->assetTypes as $type) {
-
-        // Only add a layer if the asset type is a location by default.
-        if ($type->getThirdPartySetting('farm_location', 'is_location', FALSE)) {
-
-          // Add layer for the asset type.
-          $layers[$type->id()] = [
-            'group' => $group,
-            'label' => $type->label(),
-            'asset_type' => $type->id(),
-            'filters' => $filters,
-            // @todo Color each asset type differently.
-            // This was previously provided with hook_farm_area_type_info.
-            'color' => 'orange',
-            'zoom' => TRUE,
-          ];
-        }
-      }
-
-      // Add other locations layer to the dashboard.
-      // @todo Limit this layer to only the non-location asset types.
-      // The current view doesn't allow the "type" filter to override the
-      // contextual filter that defaults to "all" asset types.
-      $layers['other_locations'] = [
-        'group' => $group,
-        'label' => $this->t('Other locations'),
-        'filters' => [
-          'is_location' => 1,
-        ],
-        'color' => 'grey',
-        'zoom' => TRUE,
-      ];
-
-      // Add the asset_type_layers behavior.
-      $event->addBehavior('asset_type_layers');
-
-      // Add map specific settings.
-      $settings[$event->getMapTargetId()]['asset_type_layers'] = $layers;
       $event->addSettings($settings);
     }
   }
