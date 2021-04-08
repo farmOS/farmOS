930df73049567197788d0f6c667c765132e9f01b
Michael Stenta <mike@mstenta.net>
2021-04-08 15:13:00 -0400

    Revert "Add a setting to configure if the map popup behavior is enabled."
    This reverts commit 19f88515437e78e671bbbf4c6229d9a014ced1ec.

--- modules/core/map/js/farmOS.map.behaviors.popup.js
+++ modules/core/map/js/farmOS.map.behaviors.popup.js
@@ -2,44 +2,37 @@
   farmOS.map.behaviors.popup = {
     attach: function (instance) {
 
-      // Add a popup if enabled on the instance. Popups are enabled by default.
-      const enabled = drupalSettings.farm_map[instance.target]?.behaviors?.popup?.enabled ?? true;
-      if (enabled) {
+      // Create a popup and add it to the instance for future reference.
+      instance.popup = instance.addPopup(function (event) {
+        var content = '';
+        var feature = instance.map.forEachFeatureAtPixel(event.pixel, function(feature, layer) { return feature; });
+        if (feature) {
 
-        // Create a popup and add it to the instance for future reference.
-        instance.popup = instance.addPopup(function (event) {
-          var content = '';
-          var feature = instance.map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
-            return feature;
-          });
-          if (feature) {
-
-            // If the feature is a cluster, then create a list of names and add it
-            // to the overall feature's description.
-            var features = feature.get('features');
-            if (features !== undefined) {
-              var names = [];
-              features.forEach(function (item) {
-                if (item.get('name') !== undefined) {
-                  names.push(item.get('name'));
-                }
-              });
-              if (names.length !== 0) {
-                feature.set('description', '<ul><li>' + names.join('</li><li>') + '</li></ul>');
+          // If the feature is a cluster, then create a list of names and add it
+          // to the overall feature's description.
+          var features = feature.get('features');
+          if (features !== undefined) {
+            var names = [];
+            features.forEach(function (item) {
+              if (item.get('name') !== undefined) {
+                names.push(item.get('name'));
               }
-              feature.set('name', names.length + ' item(s):');
+            });
+            if (names.length !== 0) {
+              feature.set('description', '<ul><li>' + names.join('</li><li>') + '</li></ul>');
             }
-
-            var name = feature.get('name') || '';
-            var description = feature.get('description') || '';
-            var measurement = instance.measureGeometry(feature.getGeometry(), instance.units);
-            if (name !== '' || measurement !== '' || description !== '') {
-              content = '<h4 class="ol-popup-name">' + name + '</h4><div class="ol-popup-measurement"><small>' + measurement + '</small></div><div class="ol-popup-description">' + description + '</div>';
-            }
+            feature.set('name', names.length + ' item(s):');
           }
-          return content;
-        });
-      }
+
+          var name = feature.get('name') || '';
+          var description = feature.get('description') || '';
+          var measurement = instance.measureGeometry(feature.getGeometry(), instance.units);
+          if (name !== '' || measurement !== '' || description !== '') {
+            content = '<h4 class="ol-popup-name">' + name + '</h4><div class="ol-popup-measurement"><small>' + measurement + '</small></div><div class="ol-popup-description">' + description + '</div>';
+          }
+        }
+        return content;
+      });
     }
   };
 }());
