595b6ff74280a3fb5fad52612fd668ddaa43e9a9
Michael Stenta <mike@mstenta.net>
2021-04-13 09:53:45 -0400

    Translate breadcrumb links.

--- modules/ui/breadcrumb/src/Breadcrumb/FarmBreadcrumbBuilder.php
+++ modules/ui/breadcrumb/src/Breadcrumb/FarmBreadcrumbBuilder.php
@@ -4,12 +4,15 @@ namespace Drupal\farm_ui_breadcrumb\Breadcrumb;
 
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\system\PathBasedBreadcrumbBuilder;
 
 /**
  * Build farmOS breadcrumbs.
  */
 class FarmBreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
+
+  use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
@@ -27,8 +30,8 @@ class FarmBreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
       case 'entity.asset.canonical':
         /** @var \Drupal\asset\Entity\AssetInterface $asset */
         $asset = $parameters->get('asset');
-        $breadcrumb->addLink(Link::createFromRoute('Records', '<front>'));
-        $breadcrumb->addLink(Link::createFromRoute('Assets', 'view.farm_asset.page'));
+        $breadcrumb->addLink(Link::createFromRoute($this->t('Records'), '<front>'));
+        $breadcrumb->addLink(Link::createFromRoute($this->t('Assets'), 'view.farm_asset.page'));
         $breadcrumb->addLink(Link::createFromRoute($asset->getBundleLabel(), 'view.farm_asset.page_type', ['arg_0' => $asset->bundle()]));
         break;
 
@@ -36,8 +39,8 @@ class FarmBreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
       case 'entity.log.canonical':
         /** @var \Drupal\log\Entity\LogInterface $log */
         $log = $parameters->get('log');
-        $breadcrumb->addLink(Link::createFromRoute('Records', '<front>'));
-        $breadcrumb->addLink(Link::createFromRoute('Logs', 'view.farm_log.page'));
+        $breadcrumb->addLink(Link::createFromRoute($this->t('Records'), '<front>'));
+        $breadcrumb->addLink(Link::createFromRoute($this->t('Logs'), 'view.farm_log.page'));
         $breadcrumb->addLink(Link::createFromRoute($log->getBundleLabel(), 'view.farm_log.page_type', ['arg_0' => $log->bundle()]));
         break;
 
@@ -45,7 +48,7 @@ class FarmBreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
       case 'entity.plan.canonical':
         /** @var \Drupal\plan\Entity\PlanInterface $plan */
         $plan = $parameters->get('plan');
-        $breadcrumb->addLink(Link::createFromRoute('Plans', 'view.farm_plan.page'));
+        $breadcrumb->addLink(Link::createFromRoute($this->t('Plans'), 'view.farm_plan.page'));
         $breadcrumb->addLink(Link::createFromRoute($plan->getBundleLabel(), 'view.farm_plan.page_type', ['arg_0' => $plan->bundle()]));
         break;
     }
