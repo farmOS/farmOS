58ced4cdb3fedcb7453b34fecf04f9e3e45e8bb6
Michael Stenta <mike@mstenta.net>
2021-05-27 13:23:31 -0400

    Expose the computed "group" asset field to Views.

---
+++ modules/asset/group/src/Plugin/views/field/AssetGroup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\farm_group\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\EntityField;
+
+/**
+ * A field that displays asset group.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("asset_group")
+ */
+class AssetGroup extends EntityField {
+
+}
