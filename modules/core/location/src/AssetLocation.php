ce152ddd31d4e30a98440fe56e2de7b421196c6c
Michael Stenta <mike@mstenta.net>
2021-06-03 08:41:47 -0400

    Do not filter out archived assets - this should be the responsibility of downstream code.

--- modules/core/location/src/AssetLocation.php
+++ modules/core/location/src/AssetLocation.php
@@ -243,9 +243,6 @@ class AssetLocation implements AssetLocationInterface {
           -- These conditions should match the values in the WHERE clause.
           AND (lfd2.status = 'done') AND (lfd2.timestamp <= :timestamp)
 
-      -- Only include assets that are not archived.
-      WHERE (afd.status != 'archived')
-
       -- Limit results to completed movement logs to the desired location that
       -- took place before the given timestamp.
       WHERE (lfd.is_movement = 1) AND (lfd.status = 'done') AND (lfd.timestamp <= :timestamp) AND (ll.location_target_id = :location_id)
