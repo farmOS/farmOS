0113020ee82180e871157a3cf4b6b9ab51274d90
Symbioquine <symbioquine@gmail.com>
2021-06-04 09:02:00 -0700

    Upgrade to farmOS-map 2.x #425
    **Why?** farmOS-map 2.x provides a slightly cleaner
    API and more modular/extensible behavior loading.

--- modules/core/map/js/farmOS.map.behaviors.geofield.js
+++ modules/core/map/js/farmOS.map.behaviors.geofield.js
@@ -1,9 +1,10 @@
 (function () {
   farmOS.map.behaviors.geofield = {
     attach: function (instance) {
-      instance.edit.wktOn('featurechange', function(wkt) {
-        console.log('here!');
-        document.querySelector('#' + instance.target).parentElement.querySelector('textarea').value = wkt;
+      instance.editAttached.then(() => {
+        instance.edit.wktOn('featurechange', function(wkt) {
+          document.querySelector('#' + instance.target).parentElement.querySelector('textarea').value = wkt;
+        });
       });
     },
 
--- modules/core/map/js/farmOS.map.behaviors.wkt.js
+++ modules/core/map/js/farmOS.map.behaviors.wkt.js
@@ -17,29 +17,38 @@
         var layer = instance.addLayer(type, opts);
       }
 
+      // Variable used to track the layer to add measurements to and to zoom in on.
+      // It is either immediately the layer created with passed-in WKT data or is later
+      // resolved to the edit layer once the edit behavior is fully attached.
+      var focusLayerPromise = Promise.resolve(layer);
+
       // If edit is true, enable drawing controls.
       if (drupalSettings.farm_map[instance.target].behaviors.wkt.edit) {
         if (layer !== undefined) {
-          instance.addBehavior('edit', { layer: layer });
+          instance.editAttached = instance.addBehavior('edit', { layer: layer });
         } else {
-          instance.addBehavior('edit');
-          var layer = instance.edit.layer;
+          instance.editAttached = instance.addBehavior('edit');
+          // Focus on the edit layer if no layer was provided
+          focusLayerPromise = instance.editAttached
+            .then(() => instance.edit.layer);
         }
 
         // Add the snappingGrid behavior.
         instance.addBehavior('snappingGrid');
       }
 
-      // Enable the line/polygon measure behavior.
-      instance.addBehavior('measure', { layer: layer });
+      focusLayerPromise.then(focusLayer => {
+        // Enable the line/polygon measure behavior.
+        instance.addBehavior('measure', { layer: focusLayer });
 
-      // If the layer has features, zoom to them.
-      // Otherwise, zoom to all vectors.
-      if (layer !== undefined) {
-        instance.zoomToLayer(layer);
-      } else {
-        instance.zoomToVectors();
-      }
+        // If the layer has features, zoom to them.
+        // Otherwise, zoom to all vectors.
+        if (focusLayer !== undefined) {
+          instance.zoomToLayer(focusLayer);
+        } else {
+          instance.zoomToVectors();
+        }
+      });
     },
     weight: 100,
   };
