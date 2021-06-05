abb0f7944a7a6f783d424d452b4cb3463405c4a9
Michael Stenta <mike@mstenta.net>
2021-06-05 13:31:12 -0400

    Remove unused use statements.

--- modules/ui/location/src/Controller/AssetReorderControllerInterface.php
+++ modules/ui/location/src/Controller/AssetReorderControllerInterface.php
@@ -3,13 +3,7 @@
 namespace Drupal\farm_ui_location\Controller;
 
 use Drupal\asset\Entity\AssetInterface;
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessResultAllowed;
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Url;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Interface for the drag and drop controller.
