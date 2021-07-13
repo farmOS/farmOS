e742293386b9b6954a8dba47596b8da8d9666eb6
paul121 <paul.weidner@gmail.com>
2021-07-13 09:10:36 -0700

    Add private_key and public fields to sensor asset.

--- modules/asset/sensor/src/Plugin/Asset/AssetType/Sensor.php
+++ modules/asset/sensor/src/Plugin/Asset/AssetType/Sensor.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\farm_sensor\Plugin\Asset\AssetType;
 
+use Drupal\data_stream\Entity\DataStream;
 use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
 
 /**
@@ -29,6 +30,27 @@ class Sensor extends FarmAssetType {
       'multiple' => TRUE,
     ];
     $fields['data_stream'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    // Private key field.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Private key'),
+      'description' => $this->t('Private key for the sensor.'),
+      'default_value_callback' => DataStream::class . '::createUniqueKey',
+      'weight' => 3,
+      'hidden' => 'view',
+    ];
+    $fields['private_key'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    // Public field.
+    $options = [
+      'type' => 'boolean',
+      'label' => $this->t('Public'),
+      'description' => $this->t('If the sensor has public access via API.'),
+      'default' => FALSE,
+      'weight' => 5,
+    ];
+    $fields['public'] = $this->farmFieldFactory->bundleFieldDefinition($options);
 
     return $fields;
   }
