926ae97372b6b18a9371acae35cf585c0364bf59
Michael Stenta <mike@mstenta.net>
2021-07-20 08:48:05 -0400

    Add automated tests of all quick form helper traits/methods.

--- modules/core/quick/tests/modules/farm_quick_test/src/QuickForm/Test.php
+++ modules/core/quick/tests/modules/farm_quick_test/src/QuickForm/Test.php
@@ -4,6 +4,10 @@ namespace Drupal\farm_quick_test\QuickForm;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\farm_quick\QuickFormBase;
+use Drupal\farm_quick\Traits\QuickAssetTrait;
+use Drupal\farm_quick\Traits\QuickLogTrait;
+use Drupal\farm_quick\Traits\QuickQuantityTrait;
+use Drupal\farm_quick\Traits\QuickTermTrait;
 
 /**
  * Test quick form.
@@ -19,6 +23,11 @@ use Drupal\farm_quick\QuickFormBase;
  * )
  */
 class Test extends QuickFormBase {
+
+  use QuickAssetTrait;
+  use QuickLogTrait;
+  use QuickQuantityTrait;
+  use QuickTermTrait;
 
   /**
    * {@inheritdoc}
@@ -39,6 +48,57 @@ class Test extends QuickFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
+    // Get the submitted value.
+    $value = $form_state->getValue('test');
+
+    // Create an asset.
+    $asset = $this->createAsset([
+      'type' => 'test',
+      'name' => $value,
+    ]);
+
+    // Create a log.
+    $log = $this->createLog([
+      'type' => 'test',
+      'name' => $value,
+      'quantity' => [
+        [
+          'measure' => 'count',
+          'value' => $value,
+          'units' => $this->t('tests'),
+        ],
+      ],
+    ]);
+
+    // Create a quantity.
+    $quantity = $this->createQuantity([
+      'measure' => 'count',
+      'value' => $value,
+      'units' => $this->t('tests'),
+      'label' => $this->t('test label'),
+    ]);
+
+    // Create a term.
+    $term1 = $this->createTerm([
+      'name' => 'test1',
+      'vocabulary' => 'test',
+    ]);
+
+    // Create a term with createOrLoadTerm().
+    $term2 = $this->createOrLoadTerm('test2', 'test');
+
+    // Load a term with createOrLoadTerm().
+    $term3 = $this->createOrLoadTerm('test2', 'test');
+
+    // Save entities to form state for automated test review.
+    $storage = [];
+    $storage['assets'][] = $asset;
+    $storage['logs'][] = $log;
+    $storage['quantities'][] = $quantity;
+    $storage['terms'][] = $term1;
+    $storage['terms'][] = $term2;
+    $storage['terms'][] = $term3;
+    $form_state->setStorage($storage);
   }
 
 }
