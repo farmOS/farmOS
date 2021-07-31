30b83735ba43ba49cb653d0c36f1935647f6acef
Paul Weidner <paul.weidner@gmail.com>
2021-07-31 12:33:29 -0700

    Limit to log types the user has access to create.

--- modules/core/log/src/Form/AssetAddLogActionForm.php
+++ modules/core/log/src/Form/AssetAddLogActionForm.php
@@ -134,7 +134,10 @@ class AssetAddLogActionForm extends ConfirmFormBase {
     }
 
     // Build list of log type options.
-    $log_types = $this->entityTypeManager->getStorage('log_type')->loadMultiple();
+    // Limit to log types the user has access to create.
+    $log_types = array_filter($this->entityTypeManager->getStorage('log_type')->loadMultiple(), function ($log_type) {
+      return $log_type->access('create', $this->currentUser());
+    });
     $log_type_options = array_map(function ($log_type) {
       return $log_type->label();
     }, $log_types);
