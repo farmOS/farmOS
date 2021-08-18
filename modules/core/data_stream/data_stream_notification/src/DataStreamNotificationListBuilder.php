4ebfea8ef4459c8c0337ce448f4231883cf7c02e
paul121 <paul.weidner@gmail.com>
2021-08-18 12:59:08 -0700

    Display enabled and disabled notifications in separate tables.

--- modules/core/data_stream/data_stream_notification/src/DataStreamNotificationListBuilder.php
+++ modules/core/data_stream/data_stream_notification/src/DataStreamNotificationListBuilder.php
@@ -13,6 +13,25 @@ class DataStreamNotificationListBuilder extends ConfigEntityListBuilder {
   /**
    * {@inheritdoc}
    */
+  public function load() {
+    $entities = [
+      'enabled' => [],
+      'disabled' => [],
+    ];
+    foreach (parent::load() as $entity) {
+      if ($entity->status()) {
+        $entities['enabled'][] = $entity;
+      }
+      else {
+        $entities['disabled'][] = $entity;
+      }
+    }
+    return $entities;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildRow(EntityInterface $notification) {
     $row = parent::buildRow($notification);
     return [
@@ -47,6 +66,40 @@ class DataStreamNotificationListBuilder extends ConfigEntityListBuilder {
         'data' => $this->t('Operations'),
       ],
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+
+    $list['#type'] = 'container';
+
+    // Add markup for the enabled table.
+    $list['enabled']['heading']['#markup'] = '<h2>' . $this->t('Enabled', [], ['context' => 'Plural']) . '</h2>';
+    $list['enabled']['table']['#empty'] = $this->t('There are no enabled notifications.');
+
+    // Add markup for the disabled table.
+    $list['disabled']['heading']['#markup'] = '<h2>' . $this->t('Disabled', [], ['context' => 'Plural']) . '</h2>';
+    $list['disabled']['table']['#empty'] = $this->t('There are no disabled notifications.');
+
+    // Build separate tables for enabled and disabled.
+    $entities = $this->load();
+    foreach (['enabled', 'disabled'] as $status) {
+      $list[$status]['table'] = [
+        '#type' => 'table',
+        '#header' => $this->buildHeader(),
+      ];
+
+      // Build a row for each entity.
+      foreach ($entities[$status] as $entity) {
+        if ($row = $this->buildRow($entity)) {
+          $list[$status]['table']['#rows'][$entity->id()] = $row;
+        }
+      }
+    }
+
+    return $list;
   }
 
 }
