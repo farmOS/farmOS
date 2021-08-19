9d046355f9f51e4b2e014dab3dee88cfc5a9a19e
paul121 <paul.weidner@gmail.com>
2021-08-18 17:28:08 -0700

    Implement a numeric notification condition.

---
+++ modules/core/data_stream/data_stream_notification/src/Plugin/DataStream/NotificationCondition/Numeric.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\data_stream_notification\Plugin\DataStream\NotificationCondition;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Numeric notification condition.
+ *
+ * @NotificationCondition(
+ *   id = "numeric",
+ *   label = @Translation("Numeric"),
+ *   context_definitions = {
+ *     "value" = @ContextDefinition("float", label = @Translation("value"))
+ *   }
+ * )
+ */
+class Numeric extends NotificationConditionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'negate' => FALSE,
+      'condition' => '<',
+      'threshold' => 0,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['condition'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Condition'),
+      '#options' => [
+        '<' => '<',
+        '>' => '>',
+      ],
+      '#default_value' => $this->configuration['condition'] ?? '<',
+    ];
+
+    $form['threshold'] = [
+      '#type' => 'number',
+      '#step' => 'any',
+      '#title' => $this->t('Threshold'),
+      '#default_value' => $this->configuration['threshold'] ?? 0,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(): bool {
+
+    // Bail if an invalid context.
+    if ($this->validateContexts()->count()) {
+      return FALSE;
+    }
+
+    // Load condition and threshold from configuration.
+    $condition = $this->configuration['condition'];
+    $threshold = $this->configuration['threshold'];
+
+    $result = FALSE;
+    $value = $this->getContextValue('value');
+    switch ($condition) {
+      case '<':
+        $result = $value < $threshold;
+        break;
+
+      case '>':
+        $result = $value > $threshold;
+        break;
+    }
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary() {
+
+    // Bail if no configuration is provided.
+    if (!isset($this->configuration['condition']) || !isset($this->configuration['threshold'])) {
+      return '';
+    }
+
+    // Get configuration.
+    $condition = $this->configuration['condition'];
+    $threshold = $this->configuration['threshold'];
+
+    // Handle negation.
+    if (!empty($this->configuration['negate'])) {
+      if ($condition === '<') {
+        return $this->t('The value is not less than @threshold.', ['@threshold' => $threshold]);
+      }
+      else {
+        return $this->t('The value is not greater than @threshold.', ['@threshold' => $threshold]);
+      }
+    }
+
+    // Standard.
+    else {
+      if ($condition === '<') {
+        return $this->t('The value is less than @threshold.', ['@threshold' => $threshold]);
+      }
+      else {
+        return $this->t('The value is greater than @threshold.', ['@threshold' => $threshold]);
+      }
+    }
+  }
+
+}
---
+++ modules/core/data_stream/data_stream_notification/tests/src/Kernel/NumericConditionTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\Tests\data_stream_notification\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the numeric notification condition.
+ *
+ * @group farm
+ */
+class NumericConditionTest extends KernelTestBase {
+
+  /**
+   * The notification condition manager interface.
+   *
+   * @var \Drupal\data_stream_notification\NotificationConditionManagerInterface
+   */
+  protected $conditionManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'data_stream_notification',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+
+    // Get the notification condition manager.
+    $this->conditionManager = $this->container->get('plugin.manager.data_stream_notification_condition');
+  }
+
+  /**
+   * Test the logic of the numeric condition plugin.
+   */
+  public function testNumericCondition() {
+
+    // Define test cases for each supported condition.
+    // Each test case is a value/result pair where the threshold is always 0.
+    $conditions = [];
+    $conditions['<'] = [
+      -5 => TRUE,
+      0 => FALSE,
+      5 => FALSE,
+    ];
+    $conditions['>'] = [
+      -5 => FALSE,
+      0 => FALSE,
+      5 => TRUE,
+    ];
+
+    // Run each test case.
+    foreach ($conditions as $condition => $test_cases) {
+
+      // Create a numeric condition instance with a threshold of 0.
+      $configuration = [
+        'threshold' => 0,
+        'condition' => $condition,
+      ];
+      /** @var \Drupal\data_stream_notification\Plugin\DataStream\NotificationCondition\NotificationConditionInterface $numeric_condition */
+      $numeric_condition = $this->conditionManager->createInstance('numeric', $configuration);
+
+      // Assert each test case.
+      foreach ($test_cases as $value => $expected) {
+        $numeric_condition->setContextValue('value', $value);
+        $this->assertEquals($expected, $numeric_condition->execute(), 'Testing: ' . $value . ' result is ' . (string) $expected);
+      }
+    }
+
+    // Test when value is non-numeric.
+    $numeric_condition->setContextValue('value', 'string');
+    $this->assertFalse($numeric_condition->execute());
+
+    // Test when there is no "value" in the context.
+    $numeric_condition->setContextValue('value', NULL);
+    $this->assertFalse($numeric_condition->execute());
+  }
+
+}
