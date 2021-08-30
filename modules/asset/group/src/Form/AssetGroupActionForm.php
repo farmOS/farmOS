0b7aa78c772368daa6dc3d51b6bfb873f4f3b287
Michael Stenta <mike@mstenta.net>
2021-08-30 15:10:14 -0400

    Use entity autocomplete widget for asset group action form.

--- modules/asset/group/src/Form/AssetGroupActionForm.php
+++ modules/asset/group/src/Form/AssetGroupActionForm.php
@@ -9,7 +9,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\Core\Url;
-use Drupal\farm_group\GroupMembershipInterface;
 use Drupal\log\Entity\Log;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -32,13 +31,6 @@ class AssetGroupActionForm extends ConfirmFormBase {
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
-
-  /**
-   * The group membership service.
-   *
-   * @var \Drupal\farm_group\GroupMembershipInterface
-   */
-  protected $groupMembership;
 
   /**
    * The current user.
@@ -68,15 +60,12 @@ class AssetGroupActionForm extends ConfirmFormBase {
    *   The tempstore factory.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
-   * @param \Drupal\farm_group\GroupMembershipInterface $group_membership
-   *   The group membership service.
    * @param \Drupal\Core\Session\AccountInterface $user
    *   The current user.
    */
-  public function __construct(PrivateTempStoreFactory $temp_store_factory, EntityTypeManagerInterface $entity_type_manager, GroupMembershipInterface $group_membership, AccountInterface $user) {
+  public function __construct(PrivateTempStoreFactory $temp_store_factory, EntityTypeManagerInterface $entity_type_manager, AccountInterface $user) {
     $this->tempStore = $temp_store_factory->get('asset_group_confirm');
     $this->entityTypeManager = $entity_type_manager;
-    $this->groupMembership = $group_membership;
     $this->user = $user;
   }
 
@@ -87,7 +76,6 @@ class AssetGroupActionForm extends ConfirmFormBase {
     return new static(
       $container->get('tempstore.private'),
       $container->get('entity_type.manager'),
-      $container->get('group.membership'),
       $container->get('current_user')
     );
   }
@@ -157,11 +145,22 @@ class AssetGroupActionForm extends ConfirmFormBase {
     ];
 
     $form['group'] = [
-      '#type' => 'select',
+      '#type' => 'entity_autocomplete',
       '#title' => $this->t('Group'),
       '#description' => $this->t('The groups to assign the asset to. Leave blank to un-assign an asset from groups.'),
-      '#options' => $this->groupMembership->groupOptions(),
-      '#multiple' => TRUE,
+      '#target_type' => 'asset',
+      '#selection_handler' => 'views',
+      '#selection_settings' => [
+        'view' => [
+          'view_name' => 'farm_group_reference',
+          'display_name' => 'entity_reference',
+        ],
+        'match_operator' => 'CONTAINS',
+        'match_limit' => 10,
+      ],
+      '#tags' => TRUE,
+      '#validate_reference' => FALSE,
+      '#maxlength' => 1024,
     ];
 
     $form['done'] = [
@@ -193,7 +192,7 @@ class AssetGroupActionForm extends ConfirmFormBase {
 
       // Load group assets.
       $groups = [];
-      $group_ids = $form_state->getValue('group');
+      $group_ids = array_column($form_state->getValue('group', []), 'target_id');
       if (!empty($group_ids)) {
         $groups = $this->entityTypeManager->getStorage('asset')->loadMultiple($group_ids);
       }
