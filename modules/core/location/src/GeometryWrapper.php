633f3e2ac52ed6e42f2deddbff55b946cf69e426
Michael Stenta <mike@mstenta.net>
2021-08-31 14:42:40 -0400

    Add references to GeoPHP maintainer suggestions to create a Geometry wrapper object for storing non-geospatial properties.

--- modules/core/location/src/GeometryWrapper.php
+++ modules/core/location/src/GeometryWrapper.php
@@ -4,6 +4,11 @@ namespace Drupal\farm_location;
 
 /**
  * An object that wraps the GeoPHP Geometry with additional properties.
+ *
+ * As suggested by the GeoPHP maintainer:
+ *
+ * @see https://github.com/phayes/geoPHP/issues/25#issuecomment-5576661
+ * @see https://github.com/phayes/geoPHP/pull/41#issuecomment-6983505
  */
 class GeometryWrapper {
 
