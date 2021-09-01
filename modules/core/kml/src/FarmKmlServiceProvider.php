ec24339bd373747fb3c24e5c52f89c9ffd7c99a2
Michael Stenta <mike@mstenta.net>
2021-09-01 11:38:13 -0400

    Change KML content type to application/vnd.google-earth.kml+xml.

--- modules/core/kml/src/FarmKmlServiceProvider.php
+++ modules/core/kml/src/FarmKmlServiceProvider.php
@@ -15,7 +15,7 @@ class FarmKmlServiceProvider implements ServiceModifierInterface {
    */
   public function alter(ContainerBuilder $container) {
     if ($container->has('http_middleware.negotiation') && is_a($container->getDefinition('http_middleware.negotiation')->getClass(), '\Drupal\Core\StackMiddleware\NegotiationMiddleware', TRUE)) {
-      $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['kml', ['application/kml']]);
+      $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['kml', ['application/vnd.google-earth.kml+xml']]);
     }
   }
 
