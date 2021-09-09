6c7c238ab8bf928aaaddda1c1a998c59082cb5b0
Michael Stenta <mike@mstenta.net>
2021-09-09 10:07:06 -0400

    Copy localized_options into the dropbutton URL options.
    This is necessary to maintain any query parameters that
    were added to the action links.

--- modules/core/ui/theme/src/Plugin/Block/FarmLocalActionsBlock.php
+++ modules/core/ui/theme/src/Plugin/Block/FarmLocalActionsBlock.php
@@ -24,6 +24,13 @@ class FarmLocalActionsBlock extends LocalActionsBlock {
     $links = [];
     foreach ($local_actions as $local_action) {
       if (!empty($local_action['#link']) && $local_action['#access']->isAllowed()) {
+
+        // Copy localized_options into the URL options.
+        // This is necessary to maintain any query parameters that were added
+        // to the action links.
+        $local_action['#link']['url']->setOptions(array_merge($local_action['#link']['url']->getOptions(), $local_action['#link']['localized_options']));
+
+        // Add the link.
         $links[] = $local_action['#link'];
       }
     }
