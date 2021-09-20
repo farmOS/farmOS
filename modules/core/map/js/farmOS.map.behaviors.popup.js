farmOS/farmOS

commit fcf32771bd2235e83c29dcf7249e4c9c4233c7b6

Only show popups for features that have a name.
This fixes the annoying issue where a mostly-blank popup
appears while drawing shapes in Geofield widget.

modules/core/map/js/farmOS.map.behaviors.popup.js
@@ -18,7 +18,7 @@
       instance.popup = instance.addPopup(function (event) {
         var content = '';
         var feature = instance.map.forEachFeatureAtPixel(event.pixel, function(feature, layer) { return feature; });
-        if (feature) {
+        if (feature && feature.get('name')) {
 
           // If the feature is a cluster, then create a list of names and add it
           // to the overall feature's description.

commit f9ecfbbf9134c83e5e750489d83f259a0eecbcf1

Popup name is required to show popup.
This fixes an issue introduced in commit fcf32771bd2235e83c29dcf7249e4c9c4233c7b6
which broke cluster popups. This still works to fix the issue
that the previous commit sought to fix (do not show popups
while drawing).

modules/core/map/js/farmOS.map.behaviors.popup.js
@@ -18,7 +18,7 @@
       instance.popup = instance.addPopup(function (event) {
         var content = '';
         var feature = instance.map.forEachFeatureAtPixel(event.pixel, function(feature, layer) { return feature; });
-        if (feature && feature.get('name')) {
+        if (feature) {
 
           // If the feature is a cluster, then create a list of names and add it
           // to the overall feature's description.
@@ -40,7 +40,9 @@
           var name = featureName(feature) || '';
           var description = feature.get('description') || '';
           var measurement = instance.measureGeometry(feature.getGeometry(), instance.units);
-          if (name !== '' || measurement !== '' || description !== '') {
+
+          // A popup name is required, along with a measurement and/or description.
+          if (name !== '' && (measurement !== '' || description !== '')) {
             content = '<h4 class="ol-popup-name">' + name + '</h4><div class="ol-popup-measurement"><small>' + measurement + '</small></div><div class="ol-popup-description">' + description + '</div>';
           }
         }