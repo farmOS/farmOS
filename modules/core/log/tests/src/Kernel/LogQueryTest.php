8127e751a840ff9d7f768d5a4136cfc2f5d4e865
paul121 <paul.weidner@gmail.com>
2021-10-01 10:01:35 -0700

    Specify accessCheck(FALSE) in LoqQueryTest.

--- modules/core/log/tests/src/Kernel/LogQueryTest.php
+++ modules/core/log/tests/src/Kernel/LogQueryTest.php
@@ -63,12 +63,12 @@ class LogQueryTest extends KernelTestBase {
     $bar_log->save();
 
     // Test that the logs are in default log query results.
-    $log_ids = $this->logQueryFactory->getQuery()->execute();
+    $log_ids = $this->logQueryFactory->getQuery()->accessCheck(FALSE)->execute();
     $this->assertContains($foo_log->id(), $log_ids, 'Log 1 appears in log query results.');
     $this->assertContains($bar_log->id(), $log_ids, 'Log 2 appears in log query results.');
 
     // Test that results can be filtered by log type.
-    $log_ids = $this->logQueryFactory->getQuery(['type' => 'foo'])->execute();
+    $log_ids = $this->logQueryFactory->getQuery(['type' => 'foo'])->accessCheck(FALSE)->execute();
     $this->assertContains($foo_log->id(), $log_ids, 'Log query results can be filtered by type.');
 
     // Set the timestamp of one log to the future.
@@ -77,7 +77,7 @@ class LogQueryTest extends KernelTestBase {
     $foo_log->save();
 
     // Test that results can be filtered by timestamp.
-    $log_ids = $this->logQueryFactory->getQuery(['timestamp' => $now])->execute();
+    $log_ids = $this->logQueryFactory->getQuery(['timestamp' => $now])->accessCheck(FALSE)->execute();
     $this->assertNotContains($foo_log->id(), $log_ids, 'Log query results can be filtered by timestamp.');
 
     // Set the status of one log to complete.
@@ -85,7 +85,7 @@ class LogQueryTest extends KernelTestBase {
     $bar_log->save();
 
     // Test that results can be filtered by status.
-    $log_ids = $this->logQueryFactory->getQuery(['status' => 'pending'])->execute();
+    $log_ids = $this->logQueryFactory->getQuery(['status' => 'pending'])->accessCheck(FALSE)->execute();
     $this->assertNotContains($bar_log->id(), $log_ids, 'Log query results can be filtered by status.');
 
     // Reference the asset in one of the logs.
@@ -93,7 +93,7 @@ class LogQueryTest extends KernelTestBase {
     $foo_log->save();
 
     // Test that results can be filtered by asset reference.
-    $log_ids = $this->logQueryFactory->getQuery(['asset' => $asset])->execute();
+    $log_ids = $this->logQueryFactory->getQuery(['asset' => $asset])->accessCheck(FALSE)->execute();
     $this->assertContains($foo_log->id(), $log_ids, 'Log that references asset is included in results.');
     $this->assertNotContains($bar_log->id(), $log_ids, 'Log that does not reference asset is not included in results.');
 
@@ -105,7 +105,7 @@ class LogQueryTest extends KernelTestBase {
     $bar_log->save();
 
     // Test that logs with the same timestamp are sorted by ID descending.
-    $log_ids = $this->logQueryFactory->getQuery()->execute();
+    $log_ids = $this->logQueryFactory->getQuery()->accessCheck(FALSE)->execute();
     $this->assertEquals($bar_log->id(), reset($log_ids), 'Logs with the same timestamp are sorted by ID descending.');
 
     // Set the timestamp of one log to the future.
@@ -114,11 +114,11 @@ class LogQueryTest extends KernelTestBase {
     $foo_log->save();
 
     // Test that logs are sorted by timestamp descending.
-    $log_ids = $this->logQueryFactory->getQuery()->execute();
+    $log_ids = $this->logQueryFactory->getQuery()->accessCheck(FALSE)->execute();
     $this->assertEquals($foo_log->id(), reset($log_ids), 'Logs are sorted by timestamp descending.');
 
     // Test that results can be limited.
-    $log_ids = $this->logQueryFactory->getQuery(['limit' => 1])->execute();
+    $log_ids = $this->logQueryFactory->getQuery(['limit' => 1])->accessCheck(FALSE)->execute();
     $this->assertEquals(1, count($log_ids), 'Log query results can be limited.');
   }
 
