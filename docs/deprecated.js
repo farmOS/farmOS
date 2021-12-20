f20e813151c7c87951ea5fa1b805fa47a761608a
Michael Stenta <mike@mstenta.net>
2021-12-20 10:55:04 -0500

    Clarify that docs are for farmOS 1.x, link to 2.x docs #115

---
+++ docs/deprecated.js
@@ -0,0 +1,3 @@
+(function ($) {
+  $('#toc-collapse').before('<div class="card" style="background-color: #FF6 !important; padding: 10px; margin: 10px 0;"><h4 style="text-align: center;">ATTENTION</h4><p><strong>These docs are for the old version of farmOS (v1). The latest docs (for v2) are available at <a href="https://docs.farmOS.org">docs.farmOS.org</a>.</strong></p></div>');
+})(jQuery)
