be5d41916e99900bd576a13f4aaf4137815b30ea
Michael Stenta <mike@mstenta.net>
2022-01-05 12:18:55 -0500

    Prepare for the next release of Gin, which changes the local storage variable name.

--- modules/core/ui/theme/js/toolbar.js
+++ modules/core/ui/theme/js/toolbar.js
@@ -1,6 +1,12 @@
 (function () {
 
   // Open the Gin toolbar by default.
+  var itemName = 'Drupal.gin.toolbarExpanded';
+  if (localStorage.getItem(itemName) === null) {
+    localStorage.setItem(itemName, 'true');
+  }
+  // @todo Remove this when new Gin version is released.
+  // Gin changed the name after 8.x-3.0-alpha37.
   itemName = 'GinSidebarOpen';
   if (localStorage.getItem(itemName) === null) {
     localStorage.setItem(itemName, 'true');
