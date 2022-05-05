3bbad9bb3616ee6dcfda2612fd9218da42fcb1a5
Paul Weidner <paul.weidner@gmail.com>
2022-05-05 11:31:11 -0700

    Remove GinSidebarOpen setting.

--- modules/core/ui/theme/js/toolbar.js
+++ modules/core/ui/theme/js/toolbar.js
@@ -5,10 +5,4 @@
   if (localStorage.getItem(itemName) === null) {
     localStorage.setItem(itemName, 'true');
   }
-  // @todo Remove this when new Gin version is released.
-  // Gin changed the name after 8.x-3.0-alpha37.
-  itemName = 'GinSidebarOpen';
-  if (localStorage.getItem(itemName) === null) {
-    localStorage.setItem(itemName, 'true');
-  }
 }());
