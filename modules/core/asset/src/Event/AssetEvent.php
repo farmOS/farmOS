7d6cb348842ed6e9b33cf6528b08a638295c4173
Michael Stenta <mike@mstenta.net>
2022-08-26 09:28:55 -0400

    Issue #3306227: Dispatch events for asset presave, insert, update, delete

---
+++ modules/core/asset/src/Event/AssetEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\asset\Event;
+
+use Drupal\Component\EventDispatcher\Event;
+use Drupal\asset\Entity\AssetInterface;
+
+/**
+ * Event that is fired by asset save, delete and clone operations.
+ */
+class AssetEvent extends Event {
+
+  const PRESAVE = 'asset_presave';
+  const INSERT = 'asset_insert';
+  const UPDATE = 'asset_update';
+  const DELETE = 'asset_delete';
+
+  /**
+   * The Asset entity.
+   *
+   * @var \Drupal\asset\Entity\AssetInterface
+   */
+  public AssetInterface $asset;
+
+  /**
+   * Constructs the object.
+   *
+   * @param \Drupal\asset\Entity\AssetInterface $asset
+   *   The Asset entity.
+   */
+  public function __construct(AssetInterface $asset) {
+    $this->asset = $asset;
+  }
+
+}
