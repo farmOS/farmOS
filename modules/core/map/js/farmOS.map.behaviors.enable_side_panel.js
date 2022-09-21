ce52efb12a9bfca662539a5a6aa9793fea60f59c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-21 09:38:08 +0800

    Delete modules directory

--- modules/core/map/js/farmOS.map.behaviors.enable_side_panel.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  // Make the built-in farmOS-map 'sidePanel' behavior attach on map instantiation
-  farmOS.map.behaviors.sidePanel = farmOS.map.namedBehaviors.sidePanel;
-
-  // Make the built-in farmOS-map 'layerSwitcherInSidePanel' behavior attach on map instantiation
-  farmOS.map.behaviors.layerSwitcherInSidePanel = farmOS.map.namedBehaviors.layerSwitcherInSidePanel;
-}());
--- modules/core/ui/theme/js/toolbar.js
+++
@@ -1,8 +0,0 @@
-(function () {
-
-  // Open the Gin toolbar by default.
-  var itemName = 'Drupal.gin.toolbarExpanded';
-  if (localStorage.getItem(itemName) === null) {
-    localStorage.setItem(itemName, 'true');
-  }
-}());
