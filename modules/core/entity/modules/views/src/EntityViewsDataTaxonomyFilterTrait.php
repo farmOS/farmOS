bef0400da4e8c19e040b84b42f3584ecccbf3929
Michael Stenta <mike@mstenta.net>
2022-09-26 17:22:48 -0400

    Fix [warning] Invalid argument supplied for foreach() EntityViewsDataTaxonomyFilterTrait.php:26 #575

--- modules/core/entity/modules/views/src/EntityViewsDataTaxonomyFilterTrait.php
+++ modules/core/entity/modules/views/src/EntityViewsDataTaxonomyFilterTrait.php
@@ -19,14 +19,37 @@ trait EntityViewsDataTaxonomyFilterTrait {
     // Configure the taxonomy_term reference field filter.
     // Logic derived form taxonomy_field_views_data_alter().
     foreach ($fields as $field) {
+
+      // If this is not a taxonomy term reference field, skip it.
       if ($field->getSettings()['target_type'] !== 'taxonomy_term') {
         continue;
       }
-      $table_name = $this->tableMapping->getFieldTableName($field->getName());
-      foreach ($data[$table_name] as $table_field_name => $field_data) {
-        $field_name = $field->getName();
-        if (isset($field_data['filter']) && strpos($table_field_name, $field_name) === 0) {
-          $data[$table_name][$table_field_name]['filter']['id'] = 'taxonomy_index_tid';
+
+      // Get the field name.
+      $field_name = $field->getName();
+
+      // Iterate through the Views data tables and columns.
+      foreach ($data as $table_name => $table_data) {
+        foreach ($table_data as $table_field_name => $field_data) {
+
+          // If this field doesn't have a filter handler, skip it.
+          if (!isset($field_data['filter'])) {
+            continue;
+          }
+
+          // Ensure that we are only altering the Views field we want.
+          // This will either be the field name itself, or the field name plus
+          // a `_target_id` suffix (depending on whether the field is a base or
+          // bundle field, single or multiple values, etc).
+          $table_field_names = [
+            $field_name,
+            $field_name . '_target_id',
+          ];
+          if (in_array($table_field_name, $table_field_names)) {
+
+            // Set the filter handler ID.
+            $data[$table_name][$table_field_name]['filter']['id'] = 'taxonomy_index_tid';
+          }
         }
       }
     }
