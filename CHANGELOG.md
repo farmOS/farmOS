15b31829aeff1c663da4f9f6161dcb3f83ffbc8d
Michael Stenta <mike@mstenta.net>
2022-09-27 13:50:36 -0400

    Revert "Prevent circular group membership #562"
    This reverts commit b495cdb840bd0bc662c1c57205ebcec8f1fe85bf.
    
    See https://github.com/farmOS/farmOS/pull/562#issuecomment-1259869273

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,7 +30,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - [Map form element #required is not enforced #560](https://github.com/farmOS/farmOS/issues/560)
 - [Incorrect translation placeholders for asset names #540](https://github.com/farmOS/farmOS/issues/540)
 - Update farmOS-map to [v2.0.5](https://github.com/farmOS/farmOS-map/releases/tag/v2.0.5) to fix [Uncaught (in promise) TypeError: o.getChangeEventType is not a function #551](https://github.com/farmOS/farmOS/issues/551)
-- [Prevent circular group membership #562](https://github.com/farmOS/farmOS/pull/562)
 - [Fix [warning] Invalid argument supplied for foreach() EntityViewsDataTaxonomyFilterTrait.php:26 #575](https://github.com/farmOS/farmOS/pull/575)
 
 ## [2.0.0-beta6] 2022-07-30
--- modules/asset/group/tests/src/Kernel/GroupTest.php
+++ modules/asset/group/tests/src/Kernel/GroupTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\farm_group\Kernel;
 
 use Drupal\asset\Entity\Asset;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\log\Entity\Log;
 use Drupal\Tests\farm_test\Kernel\FarmAssetTestTrait;
@@ -16,7 +15,6 @@ use Drupal\Tests\farm_test\Kernel\FarmEntityCacheTestTrait;
  */
 class GroupTest extends KernelTestBase {
 
-  use StringTranslationTrait;
   use FarmAssetTestTrait;
   use FarmEntityCacheTestTrait;
 
@@ -47,7 +45,6 @@ class GroupTest extends KernelTestBase {
   protected static $modules = [
     'asset',
     'log',
-    'farm_entity_views',
     'farm_field',
     'farm_group',
     'farm_group_test',
@@ -56,9 +53,7 @@ class GroupTest extends KernelTestBase {
     'farm_log_asset',
     'geofield',
     'state_machine',
-    'system',
     'user',
-    'views',
   ];
 
   /**
@@ -73,7 +68,6 @@ class GroupTest extends KernelTestBase {
     $this->installEntitySchema('log');
     $this->installEntitySchema('user');
     $this->installConfig([
-      'farm_entity_views',
       'farm_group',
       'farm_group_test',
     ]);
@@ -331,99 +325,6 @@ class GroupTest extends KernelTestBase {
     // Assert that the first group has two recursive members.
     $first_group_recursive_members = $this->groupMembership->getGroupMembers([$first_group], TRUE);
     $this->assertCorrectAssets([$animal, $second_group], $first_group_recursive_members, TRUE, 'The first group has two recursive members.');
-  }
-
-  /**
-   * Test that circular group membership is prevented.
-   */
-  public function testCircularGroupMembership() {
-
-    // Create two group assets.
-    /** @var \Drupal\asset\Entity\AssetInterface $first_group */
-    $first_group = Asset::create([
-      'type' => 'group',
-      'name' => $this->randomMachineName(),
-      'status' => 'active',
-    ]);
-    $first_group->save();
-    /** @var \Drupal\asset\Entity\AssetInterface $second_group */
-    $second_group = Asset::create([
-      'type' => 'group',
-      'name' => $this->randomMachineName(),
-      'status' => 'active',
-    ]);
-    $second_group->save();
-
-    // Start log to assign the first group to the second group.
-    /** @var \Drupal\log\Entity\LogInterface $first_log */
-    $first_log = Log::create([
-      'type' => 'test',
-      'status' => 'done',
-      'is_group_assignment' => TRUE,
-      'group' => ['target_id' => $second_group->id()],
-      'asset' => ['target_id' => $first_group->id()],
-    ]);
-
-    // Confirm that validation passes.
-    $violations = $first_log->validate();
-    $this->assertCount(0, $violations);
-
-    // Save the first log.
-    $first_log->save();
-
-    // Start a log that assigns the second group to the first group (which
-    // would cause a circular group membership).
-    /** @var \Drupal\log\Entity\LogInterface $second_log */
-    $second_log = Log::create([
-      'type' => 'test',
-      'status' => 'done',
-      'is_group_assignment' => TRUE,
-      'group' => ['target_id' => $first_group->id()],
-      'asset' => ['target_id' => $second_group->id()],
-    ]);
-
-    // Validate the second log and confirm that a circular group constraint
-    // violation was set.
-    $violations = $second_log->validate();
-    $this->assertCount(1, $violations);
-    $this->assertEquals($this->t('%asset cannot be a member of itself.', ['%asset' => $second_group->label()]), $violations[0]->getMessage());
-
-    // Create a third group asset, so we can test recursive circular group
-    // membership constraint.
-    /** @var \Drupal\asset\Entity\AssetInterface $third_group */
-    $third_group = Asset::create([
-      'type' => 'group',
-      'name' => $this->randomMachineName(),
-      'status' => 'active',
-    ]);
-    $third_group->save();
-
-    // Update the second log to reference the third group instead.
-    $second_log->set('group', ['target_id' => $third_group->id()]);
-
-    // Confirm that validation passes.
-    $violations = $second_log->validate();
-    $this->assertCount(0, $violations);
-
-    // Save the second log.
-    $second_log->save();
-
-    // Now, start a third log that assigns the third group to the first group
-    // (which would create a recursive circular group membership).
-    /** @var \Drupal\log\Entity\LogInterface $third_log */
-    $third_log = Log::create([
-      'type' => 'test',
-      'status' => 'done',
-      'is_group_assignment' => TRUE,
-      'group' => ['target_id' => $first_group->id()],
-      'asset' => ['target_id' => $third_group->id()],
-    ]);
-
-    // Validate the third log and confirm that a circular group constraint
-    // violation was set.
-    $violations = $third_log->validate();
-    $this->assertCount(1, $violations);
-    $this->assertEquals($this->t('%asset cannot be a member of itself.', ['%asset' => $third_group->label()]), $violations[0]->getMessage());
   }
 
   /**
