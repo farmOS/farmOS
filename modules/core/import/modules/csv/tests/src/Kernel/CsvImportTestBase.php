3cae2114f869c4c79b7c3c604893a62b70b36e88
Michael Stenta <mike@mstenta.net>
2023-08-18 14:40:13 -0400

    Provide a base class for CSV import kernel tests.

---
+++ modules/core/import/modules/csv/tests/src/Kernel/CsvImportTestBase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\Tests\farm_import_csv\Kernel;
+
+use Drupal\Tests\migrate\Kernel\MigrateTestBase;
+
+/**
+ * Base class for farmOS CSV importer kernel tests.
+ *
+ * @group farm
+ */
+class CsvImportTestBase extends MigrateTestBase {
+
+  /**
+   * The migration manager.
+   *
+   * @var \Drupal\migrate\Plugin\MigrationPluginManagerInterface
+   */
+  protected $migrationManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'asset',
+    'farm_import',
+    'farm_import_csv',
+    'farm_import_csv_test',
+    'filter',
+    'log',
+    'migrate',
+    'migrate_plus',
+    'migrate_source_csv',
+    'migrate_source_ui',
+    'migrate_tools',
+    'taxonomy',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+    $this->migrationManager = $this->container->get('plugin.manager.migration');
+  }
+
+}
