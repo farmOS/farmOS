3915160a6d10fba7a67417c2556e09b8c678c286
Michael Stenta <mike@mstenta.net>
2023-08-21 17:07:59 -0400

    Add support for importing log quantities.

--- modules/core/import/modules/csv/tests/src/Kernel/CsvImportTestBase.php
+++ modules/core/import/modules/csv/tests/src/Kernel/CsvImportTestBase.php
@@ -26,14 +26,18 @@ class CsvImportTestBase extends MigrateTestBase {
    */
   protected static $modules = [
     'asset',
+    'entity_reference_revisions',
     'farm_entity_fields',
     'farm_field',
     'farm_format',
     'farm_import',
     'farm_import_csv',
     'farm_import_csv_test',
+    'farm_log_quantity',
+    'farm_quantity_standard',
     'file',
     'filter',
+    'fraction',
     'image',
     'log',
     'migrate',
@@ -41,6 +45,8 @@ class CsvImportTestBase extends MigrateTestBase {
     'migrate_source_csv',
     'migrate_source_ui',
     'migrate_tools',
+    'options',
+    'quantity',
     'state_machine',
     'system',
     'taxonomy',
@@ -56,9 +62,10 @@ class CsvImportTestBase extends MigrateTestBase {
     $this->migrationManager = $this->container->get('plugin.manager.migration');
     $this->installEntitySchema('asset');
     $this->installEntitySchema('log');
+    $this->installEntitySchema('quantity');
     $this->installEntitySchema('taxonomy_term');
     $this->installEntitySchema('user');
-    $this->installConfig(['farm_format']);
+    $this->installConfig(['farm_format', 'farm_quantity_standard']);
 
     // Run tests as the user 1 to avoid permissions issues.
     $this->setUpCurrentUser(['uid' => 1]);
--- modules/core/import/modules/csv/tests/src/Kernel/LogCsvImportTest.php
+++ modules/core/import/modules/csv/tests/src/Kernel/LogCsvImportTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\farm_import_csv\Kernel;
 
 use Drupal\log\Entity\Log;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Tests for log CSV importers.
@@ -37,25 +38,49 @@ class LogCsvImportTest extends CsvImportTestBase {
     // Execute the migration.
     $this->executeMigration($migration);
 
+    // Confirm that two taxonomy terms were created with the expected values.
+    $terms = Term::loadMultiple();
+    $this->assertCount(2, $terms);
+    $this->assertEquals('bulbs', $terms[1]->label());
+    $this->assertEquals('lbs', $terms[2]->label());
+
     // Confirm that logs have been created with the expected values.
     $logs = Log::loadMultiple();
     $this->assertCount(3, $logs);
     $expected_values = [
       1 => [
-        'timestamp' => 1689343200,
         'name' => 'Harvest garlic',
+        'timestamp' => 1689343200,
+        'quantity' => [
+          'measure' => 'count',
+          'value' => '200',
+          'units' => 'bulbs',
+          'label' => 'total',
+        ],
         'notes' => 'Great big bulbs',
         'status' => 'done',
       ],
       2 => [
-        'timestamp' => 1692021600,
         'name' => 'Harvest potatoes',
+        'timestamp' => 1692021600,
+        'quantity' => [
+          'measure' => 'weight',
+          'value' => '80',
+          'units' => 'lbs',
+          'label' => '',
+        ],
         'notes' => 'Heavy harvest',
         'status' => 'done',
       ],
       3 => [
-        'timestamp' => 1694700000,
         'name' => 'Harvest onions',
+        'timestamp' => 1694700000,
+        'quantity' => [
+          'measure' => 'weight',
+          'value' => '',
+          'units' => 'lbs',
+          'label' => '',
+        ],
         'notes' => 'Small bulbs from weed pressure',
         'status' => 'pending',
       ],
@@ -63,6 +88,10 @@ class LogCsvImportTest extends CsvImportTestBase {
     foreach ($logs as $id => $log) {
       $this->assertEquals('harvest', $log->bundle());
       $this->assertEquals($expected_values[$id]['name'], $log->label());
+      $this->assertEquals($expected_values[$id]['quantity']['measure'], $log->get('quantity')->referencedEntities()[0]->get('measure')->value);
+      $this->assertEquals($expected_values[$id]['quantity']['value'], $log->get('quantity')->referencedEntities()[0]->get('value')->decimal);
+      $this->assertEquals($expected_values[$id]['quantity']['units'], $log->get('quantity')->referencedEntities()[0]->get('units')->referencedEntities()[0]->label());
+      $this->assertEquals($expected_values[$id]['quantity']['label'], $log->get('quantity')->referencedEntities()[0]->get('label')->value);
       $this->assertEquals($expected_values[$id]['timestamp'], $log->get('timestamp')->value);
       $this->assertEquals($expected_values[$id]['notes'], $log->get('notes')->value);
       $this->assertEquals('default', $log->get('notes')->format);
