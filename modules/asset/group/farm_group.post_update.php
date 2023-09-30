8f2c465de99944d4ca1c7b267d9ad707d1900318
Michael Stenta <mike@mstenta.net>
2023-09-30 15:02:01 -0400

    Replace Group action with Group quick form action.

---
+++ modules/asset/group/farm_group.post_update.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Post update hooks for the farm_group module.
+ */
+
+use Drupal\system\Entity\Action;
+
+/**
+ * Uninstall system.action.asset_group_action.
+ */
+function farm_group_post_update_uninstall_asset_group_action(&$sandbox) {
+  $config = Action::load('asset_group_action');
+  $config->delete();
+}
