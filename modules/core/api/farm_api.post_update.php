7b8beddd9ead9ac5e3a5467b8158fcde66e5dc80
Paul Weidner <paul.weidner@gmail.com>
2023-10-30 12:30:53 -0700

    Use simple oauth static scope provider by default

--- modules/core/api/farm_api.post_update.php
+++ modules/core/api/farm_api.post_update.php
@@ -22,3 +22,19 @@ function farm_api_post_update_remove_consumer_fields(&$sandbox = NULL) {
     $entity_definition_update_manager->uninstallFieldStorageDefinition($roles_field_definition);
   }
 }
+
+/**
+ * Enable static oauth2 scopes.
+ */
+function farm_api_post_update_enable_static_oauth2_scopes(&$sandbox = NULL) {
+
+  // Enable static scope module.
+  if (!\Drupal::service('module_handler')->moduleExists('simple_oauth_static_scope')) {
+    \Drupal::service('module_installer')->install(['simple_oauth_static_scope']);
+  }
+
+  // Use static scope provider.
+  $simple_oauth_settings = \Drupal::configFactory()->getEditable('simple_oauth.settings');
+  $simple_oauth_settings->set('scope_provider', 'static');
+  $simple_oauth_settings->save();
+}
