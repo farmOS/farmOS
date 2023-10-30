0673bebbdae3962cda3d5a5c2e302532d719f349
Paul Weidner <paul.weidner@gmail.com>
2023-10-30 15:51:13 -0700

    Set default scopes for fieldkit consumer

--- modules/core/fieldkit/farm_fieldkit.post_update.php
+++ modules/core/fieldkit/farm_fieldkit.post_update.php
@@ -5,6 +5,8 @@
  * Post update functions for farm_fieldkit module.
  */
 
+use Drupal\simple_oauth\Oauth2ScopeInterface;
+
 /**
  * Enable simple oauth password grant.
  */
@@ -15,6 +17,14 @@ function farm_fieldkit_post_update_enable_password_grant(&$sandbox = NULL) {
     \Drupal::service('module_installer')->install(['simple_oauth_password_grant']);
   }
 
+  // Check for default role scopes.
+  /** @var \Drupal\simple_oauth\Oauth2ScopeProviderInterface $scope_provider */
+  $scope_provider = \Drupal::service('simple_oauth.oauth2_scope.provider');
+  $scopes = $scope_provider->loadMultiple(['farm_manager', 'farm_worker']);
+  $scope_ids = array_map(function (Oauth2ScopeInterface $scope) {
+    return $scope->id();
+  }, $scopes);
+
   // Update existing fieldkit consumer.
   $consumers = \Drupal::entityTypeManager()->getStorage('consumer')
     ->loadByProperties(['client_id' => 'fieldkit']);
@@ -23,6 +33,7 @@ function farm_fieldkit_post_update_enable_password_grant(&$sandbox = NULL) {
     $fieldkit = reset($consumers);
     $fieldkit->set('user_id', NULL);
     $fieldkit->set('grant_types', ['refresh_token', 'password']);
+    $fieldkit->set('scopes', array_values($scope_ids));
     $fieldkit->save();
   }
 }
