4226394e745b9cf46a2bdc61a26984cac0324834
Paul Weidner <paul.weidner@gmail.com>
2023-10-30 14:52:24 -0700

    Update fieldkit consumer to only use refresh_token and password grants

--- modules/core/fieldkit/farm_fieldkit.post_update.php
+++ modules/core/fieldkit/farm_fieldkit.post_update.php
@@ -9,7 +9,20 @@
  * Enable simple oauth password grant.
  */
 function farm_fieldkit_post_update_enable_password_grant(&$sandbox = NULL) {
+
+  // Enable password grant module.
   if (!\Drupal::service('module_handler')->moduleExists('simple_oauth_password_grant')) {
     \Drupal::service('module_installer')->install(['simple_oauth_password_grant']);
+  }
+
+  // Update existing fieldkit consumer.
+  $consumers = \Drupal::entityTypeManager()->getStorage('consumer')
+    ->loadByProperties(['client_id' => 'fieldkit']);
+  if (!empty($consumers)) {
+    /** @var \Drupal\consumers\Entity\ConsumerInterface $fieldkit */
+    $fieldkit = reset($consumers);
+    $fieldkit->set('user_id', NULL);
+    $fieldkit->set('grant_types', ['refresh_token', 'password']);
+    $fieldkit->save();
   }
 }
