504db8624e1aed6d4622c5aea57ae124fb0fd43c
Michael Stenta <mike@mstenta.net>
2025-03-24 14:37:21 -0400

    Configure imagick to disable strip_metadata.

--- modules/core/image/farm_image.post_update.php
+++ modules/core/image/farm_image.post_update.php
@@ -8,7 +8,7 @@
 declare(strict_types=1);
 
 /**
- * Install the imagick module, if the PHP extension is available.
+ * Install and configure the imagick module, if the PHP extension is available.
  */
 function farm_image_post_update_install_imagick() {
   if (extension_loaded('imagick')) {
@@ -17,5 +17,12 @@ function farm_image_post_update_install_imagick() {
     if (!\Drupal::service('module_handler')->moduleExists('imagick')) {
       \Drupal::service('module_installer')->install(['imagick']);
     }
+
+    // Configure imagick to disable strip_metadata.
+    // This ensures that EXIF data is preserved in derivative images, which
+    // allows browsers to auto-orient them if they have Orientation data.
+    $config = \Drupal::configFactory()->getEditable('imagick.config');
+    $config->set('strip_metadata', FALSE);
+    $config->save();
   }
 }
