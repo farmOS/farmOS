bd0bffce1d1394e6996dd9c26d443bd4f1358ec1
Michael Stenta <mike@mstenta.net>
2026-03-02 15:24:56 -0500

    Issue #3576633: Deprecate asset/organization/quantity entity events

--- modules/core/asset/src/Event/AssetEvent.php
+++ modules/core/asset/src/Event/AssetEvent.php
@@ -8,7 +8,11 @@ use Drupal\Component\EventDispatcher\Event;
 use Drupal\asset\Entity\AssetInterface;
 
 /**
- * Event that is fired by asset save, delete and clone operations.
+ * Event that is fired by asset entity operations.
+ *
+ * @deprecated in farm:4.1.0 and is removed from farm:5.0.0.
+ *   Use Drupal core entity hooks instead.
+ * @see https://www.drupal.org/node/3576637
  */
 class AssetEvent extends Event {
 
