708afceb6210801dd9f7d5167f2482eadbf353df
Michael Stenta <mike@mstenta.net>
2026-04-22 21:27:04 -0400

    Add a unified asset+log search

--- modules/core/ui/search/src/Hook/ThemeHooks.php
+++ modules/core/ui/search/src/Hook/ThemeHooks.php
@@ -2,14 +2,17 @@
 
 declare(strict_types=1);
 
-namespace Drupal\farm_setup\Hook;
+namespace Drupal\farm_ui_search\Hook;
 
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
- * Theme hook implementations for farm_setup.
+ * Theme hook implementations for farm_ui_search.
  */
 class ThemeHooks {
+
+  use StringTranslationTrait;
 
   /**
    * Implements hook_farm_dashboard_panes().
@@ -17,10 +20,11 @@ class ThemeHooks {
   #[Hook('farm_dashboard_panes')]
   public function farmDashboardPanes() {
     return [
-      'setup' => [
-        'block' => 'farm_setup',
+      'search' => [
+        'view' => 'farm_search',
+        'view_display_id' => 'block',
         'region' => 'top',
-        'weight' => -100,
+        'weight' => -90,
       ],
     ];
   }
